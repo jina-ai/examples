43b19176f5e0ab25b0d66e0544ca37dea92511a1
Han Xiao <han.xiao@jina.ai>
2020-02-17 09:09:30 +0100

    initial commit

---
+++ tumblr-gif-search/app.py
@@ -0,0 +1,72 @@
+import glob
+import json
+import os
+import random
+from datetime import datetime
+
+from google.protobuf.json_format import MessageToDict
+from jina.flow import Flow
+
+RUN_MODE = 'debug-index'
+MODEL_ID = '20200202224053'
+
+WORK_DIR = '/Volumes/TOSHIBA-4T/model/'
+GIF_BLOB = '/Volumes/TOSHIBA-4T/dataset/thumblr-gif-data/*.gif'  # 'data/*.gif'
+TIMESTAMP = datetime.now().strftime('%Y%m%d%H%M%S')
+
+os.environ['TEST_WORKDIR'] = WORK_DIR + MODEL_ID
+os.environ['GNES_LOG_FORMAT'] = 'JSON'
+os.environ['REPLICAS'] = '3'
+# os.environ['GRPC_VERBOSITY'] = 'debug'
+# os.environ['GRPC_TRACE']='tcp'
+do_index = True
+
+if RUN_MODE == 'debug-index':
+    replicas = 1
+    num_docs = 100
+elif RUN_MODE == 'index':
+    replicas = 4
+    num_docs = 200000
+elif RUN_MODE == 'debug-query' or RUN_MODE == 'query':
+    do_index = False
+    replicas = 1
+    num_docs = 10
+else:
+    raise AttributeError(RUN_MODE)
+
+if RUN_MODE.endswith('index'):
+    os.environ['TEST_WORKDIR'] = WORK_DIR + TIMESTAMP
+    os.makedirs(os.environ['TEST_WORKDIR'], exist_ok=True)
+
+f = Flow.load_config('index-flow.yml')
+
+q = Flow.load_config('index-query.yml')
+
+
+def print_result(resp, fp):
+    for d in resp.search.docs:
+        v = MessageToDict(d, including_default_value_fields=True)
+        v['metaInfo'] = d.raw_bytes.decode()
+        for k, kk in zip(v['topkResults'], d.topk_results):
+            k['matchDoc']['metaInfo'] = kk.match_doc.raw_bytes.decode()
+            # k['score']['explained'] = json.loads(kk.score.explained)
+        fp.write(json.dumps(v, sort_keys=True) + '\n')
+
+
+if do_index:
+    # index
+    bytes_gen = (g.encode() for g in glob.glob(GIF_BLOB)[:num_docs])
+
+    with f.build(backend='process') as fl:
+        fl.index(bytes_gen, batch_size=64)
+else:
+    # query
+    with open(os.environ['TEST_WORKDIR'] + '/topk.json', 'w', encoding='utf8') as fp:
+        ppr = lambda x: print_result(x, fp)
+
+        if RUN_MODE == 'query':
+            bytes_gen = (g.encode() for g in random.sample(glob.glob(GIF_BLOB), num_docs))
+        else:
+            bytes_gen = (g.encode() for g in glob.glob(GIF_BLOB)[:num_docs])
+        with q.build(backend='process') as fl:
+            fl.search(bytes_gen, callback=ppr, top_k=60, batch_size=32)
---
+++ tumblr-gif-search/gif_download.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+import time
+
+import aiofiles
+import aiohttp
+
+batch_size = 30
+
+with open('data/tgif-v1.0.tsv') as fp:
+    all_urls = [v.split('\t')[0] for v in fp]
+    print('%d urls' % len(all_urls))
+    all_urls = [v for v in all_urls if not os.path.exists('data/%s' % v.split('/')[-1])]
+    print('%d urls' % len(all_urls))
+
+
+async def download(url):
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        async with session.get(url) as r:
+            print('downloading %s' % url)
+            gif_data = await r.read()
+        gif_name = 'data/%s' % url.split('/')[-1]
+        print('save it to %s' % gif_name)
+        async with aiofiles.open(gif_name, 'wb') as fp:
+            await fp.write(gif_data)
+
+
+async def main(urls):
+    tasks = [asyncio.create_task(download(u)) for u in urls]
+    await asyncio.gather(*tasks)
+
+
+for _ in range(0, len(all_urls), batch_size):
+    asyncio.run(main(all_urls[_:(_ + batch_size)]))
+    time.sleep(1)
