91da8fd255a038180121c2af9fcbc0cb79354959
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-03-07 15:03:32 +0800

    feat(urbandict): add indexing part

---
+++ urbandict-search/index.py
@@ -0,0 +1,67 @@
+import os
+import csv
+import json
+from collections import defaultdict
+from jina.flow import Flow
+
+
+def read_data(fn, max_sample_size=1000):
+    illegal_counts = 0
+    with open(fn, "r") as f:
+        r = csv.reader(f)
+        word2def = defaultdict(list)
+        for idx, l in enumerate(r):
+            if idx == 0:
+                continue
+            if len(l) != 6:
+                illegal_counts += 1
+                continue
+            _, word, up_votes, down_votes, _, word_def = l
+            up_votes = int(up_votes)
+            down_votes = int(down_votes)
+            weight = up_votes * 1.0 / down_votes if down_votes != 0 else up_votes
+            if len(word_def) == 0:
+                continue
+            word2def[word].append({"text": word_def, "weight": weight})
+    print("illegal counts: {}/{}".format(illegal_counts, idx))
+    print("word2def size: {}/{}".format(len(word2def), idx))
+    count = 0
+    result = []
+    for k, v in word2def.items():
+        json_dict = {"word": k, "def": v}
+        if len(v) == 0:
+            continue
+        count += 1
+        if count >= max_sample_size:
+            break
+        result.append(("{}".format(json.dumps(json_dict, ensure_ascii=False))).encode("utf8"))
+    for r in result:
+        yield r
+
+
+def main():
+    workspace_path = '/tmp/jina/urbandict'
+    os.environ['TMP_WORKSPACE'] = workspace_path
+    data_fn = os.path.join(workspace_path, "urbandict-word-defs.csv")
+
+    flow = (Flow().add(
+        name='extractor', yaml_path='yaml/extractor.yml'
+    ).add(
+        name='meta_doc_indexer', yaml_path='yaml/meta_doc_indexer.yml'
+    ).add(
+        name='encoder', yaml_path='yaml/encoder.yml',
+        recv_from='extractor'
+    ).add(
+        name='compound_chunk_indexer', yaml_path='yaml/compound_chunk_indexer.yml',
+        recv_from='encoder'
+    ).add(
+        name='merger', yaml_path='yaml/merger.yml',
+        recv_from=['meta_doc_indexer', 'compound_chunk_indexer']
+    ))
+
+    with flow.build(runtime="thread") as f:
+        f.index(raw_bytes=read_data(data_fn))
+
+
+if __name__ == '__main__':
+    main()
---
+++ urbandict-search/yaml/customized_executors.py
@@ -0,0 +1,36 @@
+from jina.executors.transformers import BaseSegmenter
+from jina.executors.encoders import BaseTextEncoder
+import json
+import numpy as np
+
+
+class DictEntryExtractor(BaseSegmenter):
+    def transform(self, doc_id, raw_bytes, *args, **kwargs):
+        json_str = raw_bytes.decode("utf8")
+        json_dict = json.loads(json_str)
+        word = json_dict["word"]
+        chunk_list = []
+        for idx, d in enumerate(json_dict["def"]):
+            chunk_text = d["text"]
+            chunk_weight = d["weight"]
+            chunk_list.append(
+                {
+                    "raw_bytes": chunk_text.encode("utf8"),
+                    "chunk_id": idx,
+                    "doc_id": doc_id,
+                    "offset": idx,
+                    "weight": chunk_weight,
+                    "length": len(chunk_text),
+                    "text": chunk_text
+                }
+            )
+        return chunk_list
+
+
+class RandomEncoder(BaseTextEncoder):
+    def encode(self, data, *args, **kwargs):
+        num_chunks = data.shape[0]
+        emb_dim = 128
+        emb_result = np.random.rand(num_chunks, emb_dim)
+        self.logger.info("encode random embedding for the sentences")
+        return emb_result
