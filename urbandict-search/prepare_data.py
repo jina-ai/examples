d2fcad70970988d2c5a034a70f1f071eba7ddf1e
xiong-ma <xiong.ma@jina.ai>
2020-03-20 14:32:15 +0800

    optimize create workpath folder

--- urbandict-search/prepare_data.py
+++ urbandict-search/prepare_data.py
@@ -2,9 +2,12 @@ import os
 import zipfile
 
 data_fn = "urbandict-word-defs.csv"
-workspace_path = '/tmp/jina/urbandict'
+root_path = '/tmp/jina/'
+demo_name = 'urbandict'
+workspace_path = os.path.join(root_path, demo_name)
 if not os.path.exists(workspace_path):
     print("data output dir: {}".format(workspace_path))
+    os.mkdir(root_path)
     os.mkdir(workspace_path)
 
 tmp_data_path = os.path.join(workspace_path, data_fn)
