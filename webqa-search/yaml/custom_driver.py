68809abf3a2089884e293efd28146b74f2d3692b
Xiong Ma <xiong.ma@jina.ai>
2020-03-30 14:26:24 +0800

    add query flow
    Former-commit-id: 27b2b7233bb0597db5867f1b7a5bf0d410772966 [formerly f61359263ededd5f7e6289c19f84af44c87d9eba]
    Former-commit-id: e409be88f2f34011f20e5ba953c90eeef48649f9

--- webqa-search/yaml/custom_driver.py
+++ webqa-search/yaml/custom_driver.py
@@ -1,11 +1,28 @@
 
-import numpy as np
 from jina.drivers import BaseExecutableDriver
 from jina.drivers.helper import extract_chunks
-import json
+from jina.drivers.helper import blob2array
+import numpy as np
+
+class AnswerSearchDriver(BaseExecutableDriver):
+    """Fill in the chunk-level top-k results using the AnswerIndexer
+
+    """
+
+    def __call__(self, *args, **kwargs):
+        for d in self.req.docs:
+            chunk = d.chunks[0] # when query, every document just has one chunk
+            title_topk = [(c.match_chunk.chunk_id, c.score.value) for c in chunk.topk_results]
+            texts, topk_answer_ids, topk_dists = self.exec_fn(np.array([blob2array(chunk.embedding)]), title_topk,
+                                                               self.req.top_k)
+
+            for c, text, answer_id, dist in zip(chunk.topk_results, texts, topk_answer_ids, topk_dists):
+                c.match_chunk.text = text
+                c.match_chunk.chunk_id = answer_id
+                c.score.value = dist
 
 class TitleIndexDriver(BaseExecutableDriver):
-    """Extract title-level embeddings and add it to the executor
+    """Extract chunk-level embeddings and add it to the executor
 
     """
 
@@ -21,8 +38,3 @@ class TitleIndexDriver(BaseExecutableDriver):
         if chunk_pts:
             self.exec_fn(np.array([c.doc_id for c in chunk_pts]), np.stack(embed_vecs))
 
-class AnswerIndexDriver(BaseExecutableDriver):
-
-    def __call__(self, *args, **kwargs):
-        for d in self.req.docs:
-            self.exec_fn(d)
