5d480a1c187b7c3408edc71a6a20e13d0fc14110
Han Xiao <han.xiao@jina.ai>
2020-04-14 15:40:57 +0200

    update according to latest api

--- tumblr-gif-search/craft/gif2chunk.py
+++ tumblr-gif-search/craft/gif2chunk.py
@@ -1,24 +1,41 @@
+import io
+import json
+from typing import Dict
+
 import numpy as np
 from PIL import Image
+from craft.gif_reader import get_frames
+from jina.executors.crafters import BaseDocCrafter
 from jina.executors.crafters import BaseSegmenter
 
-from craft.gif_reader import get_frames
+
+class GifNameRawSplit(BaseDocCrafter):
+
+    def craft(self, raw_bytes, *args, **kwargs) -> Dict:
+        file_name, raw_bytes = raw_bytes.split(b'JINA_DELIM')
+        return dict(raw_bytes=raw_bytes, meta_info=file_name)
 
 
 class GifPreprocessor(BaseSegmenter):
-    def __init__(self, img_shape: int = 96, every_k_frame: int = 1, max_frame: int = None, *args, **kwargs):
+    def __init__(self, img_shape: int = 96, every_k_frame: int = 1, max_frame: int = None, from_bytes: bool = False,
+                 *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.img_shape = img_shape
         self.every_k_frame = every_k_frame
         self.max_frame = max_frame
+        self.from_bytes = from_bytes
 
     def craft(self, raw_bytes, doc_id):
         result = []
         try:
-            im = Image.open(raw_bytes.decode())
+            if self.from_bytes:
+                im = Image.open(io.BytesIO(raw_bytes))
+            else:
+                im = Image.open(raw_bytes.decode())
             idx = 0
             for frame in get_frames(im):
                 try:
+
                     if idx % self.every_k_frame == 0 and (
                             (self.max_frame is not None and idx < self.max_frame) or self.max_frame is None):
                         new_frame = frame.convert('RGB').resize([self.img_shape, ] * 2)
