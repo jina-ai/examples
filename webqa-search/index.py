cbd15d9c1a7edc96739b542de2220d1a45974135
Xiong Ma <xiong.ma@jina.ai>
2020-04-17 18:37:46 +0800

    optimize query output

--- webqa-search/index.py
+++ webqa-search/index.py
@@ -14,29 +14,30 @@ def read_data(fn):
             if item['qid'] not in items.keys():
                 items[item['qid']] = {}
                 items[item['qid']]['title'] = item['title']
-                items[item['qid']]['answers'] = [{'answer_id': item['answer_id'], 'content': item['content']}]
+                items[item['qid']]['answers'] = [{'content': item['content']}]
             else:
-                items[item['qid']]['answers'].append({'answer_id': item['answer_id'], 'content': item['content']})
+                items[item['qid']]['answers'].append({'content': item['content']})
 
     result = []
-    for qid, value in items.items():
-        value['qid'] = qid
+    for _, value in items.items():
         result.append(("{}".format(json.dumps(value, ensure_ascii=False))).encode("utf-8"))
 
-    for item in result[:100]:
+    for item in result[:50000]:
         yield item
 
 
 def main():
-    workspace_path = '/tmp/jina/webqa'
+    workspace_path = '/home/cally/jina/webqa'
     os.environ['TMP_WORKSPACE'] = workspace_path
-    data_fn = os.path.join(workspace_path, "web_text_zh_valid.json")
+    data_fn = os.path.join(workspace_path, "web_text_zh_train.json")
     flow = (Flow().add(name='title_extractor', yaml_path='images/title_extractor/title_extractor.yml')
-        .add(name='tmd_indexer', yaml_path='images/title_meta_doc_indexer/title_meta_doc_indexer.yml', needs='gateway')
-        .add(name='title_encoder', yaml_path='images/encoder/encoder.yml', needs='title_extractor', timeout_ready=60000)
-        .add(name='tcc_indexer', yaml_path='images/title_compound_chunk_indexer/title_compound_chunk_indexer.yml',
-             needs='title_encoder')
-        .join(['tcc_indexer', 'tmd_indexer']))
+            .add(name='tmd_indexer', yaml_path='images/title_meta_doc_indexer/title_meta_doc_indexer.yml',
+                 needs='gateway')
+            .add(name='title_encoder', yaml_path='images/encoder/encoder.yml', needs='title_extractor',
+                 timeout_ready=60000, replicas=3)
+            .add(name='tcc_indexer', yaml_path='images/title_compound_chunk_indexer/title_compound_chunk_indexer.yml',
+                 needs='title_encoder')
+            .join(['tcc_indexer', 'tmd_indexer']))
     with flow.build() as f:
         f.index(raw_bytes=read_data(data_fn))
 
--- webqa-search/query.py
+++ webqa-search/query.py
@@ -16,36 +16,42 @@ def read_data(fn):
             if item['qid'] not in items.keys():
                 items[item['qid']] = {}
                 items[item['qid']]['title'] = item['title']
-                items[item['qid']]['answers'] = [{'answer_id': item['answer_id'], 'content': item['content']}]
-            else:
-                items[item['qid']]['answers'].append({'answer_id': item['answer_id'], 'content': item['content']})
 
     result = []
-    for qid, value in items.items():
-        value['qid'] = qid
+    for _, value in items.items():
         result.append(("{}".format(json.dumps(value, ensure_ascii=False))).encode("utf-8"))
 
     for item in result[:10]:
         yield item
 
-
 def main():
-    workspace_path = '/tmp/jina/webqa'
+    workspace_path = '/home/cally/jina/webqa'
     os.environ['TMP_WORKSPACE'] = workspace_path
     data_fn = os.path.join(workspace_path, "web_text_zh_valid.json")
     flow = (Flow().add(name='extractor', yaml_path='images/title_extractor/title_extractor.yml', needs='gateway')
-        .add(name='encoder', yaml_path='images/encoder/encoder.yml', needs="extractor", timeout_ready=60000)
-        .add(name='tcc_indexer',yaml_path='images/title_compound_chunk_indexer/title_compound_chunk_indexer.yml',
-             needs='encoder')
-        .add(name='ranker', yaml_path='images/ranker/ranker.yml', needs='tcc_indexer')
-        .add(name='tmd_indexer', yaml_path='images/title_meta_doc_indexer/title_meta_doc_indexer.yml',needs='ranker'))
+            .add(name='encoder', yaml_path='images/encoder/encoder.yml', needs="extractor", timeout_ready=60000)
+            .add(name='tcc_indexer', yaml_path='images/title_compound_chunk_indexer/title_compound_chunk_indexer.yml',
+                 needs='encoder')
+            .add(name='ranker', yaml_path='images/ranker/ranker.yml', needs='tcc_indexer')
+            .add(name='tmd_indexer', yaml_path='images/title_meta_doc_indexer/title_meta_doc_indexer.yml',
+                 needs='ranker'))
 
     def print_topk(resp, fp):
         for d in resp.search.docs:
             v = MessageToDict(d, including_default_value_fields=True)
-            v['metaInfo'] = d.raw_bytes.decode()
+            v['query'] = eval(d.raw_bytes.decode())['title']
+
+            pops = ('rawBytes', 'weight', 'length', 'chunks')
+
             for k, kk in zip(v['topkResults'], d.topk_results):
-                k['matchDoc']['metaInfo'] = kk.match_doc.raw_bytes.decode()
+                k['matchDoc']['metaInfo'] = eval(kk.match_doc.raw_bytes.decode())
+                for pop in pops:
+                    k['matchDoc'].pop(pop)
+
+                k['matchDoc'].pop('topkResults')
+            for pop in pops:
+                v.pop(pop)
+            v.pop('metaInfo')
             fp.write(json.dumps(v, sort_keys=True, indent=4, ensure_ascii=False) + "\n")
 
     with open("{}/query_result.json".format(os.environ['TMP_WORKSPACE']), "w") as fp:
