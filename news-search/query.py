a1b00b9ff074f5d1fb814b1158dc7ca935c1c78e
xiong ma <maxiong@juexiaotime.com>
2020-04-17 09:53:32 +0800

    reformat code

--- news-search/query.py
+++ news-search/query.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 
@@ -25,23 +24,17 @@ def read_data(fn):
     for item in result[:10]:
         yield item
 
+
 def main():
     workspace_path = '/home/cally/jina/news'
     os.environ['TMP_WORKSPACE'] = workspace_path
     data_fn = os.path.join(workspace_path, "news2016zh_valid.json")
-    flow = Flow().add(
-        name='extractor', yaml_path='images/extractor/extractor.yml'
-    ).add(
-        name='encoder', yaml_path='images/encoder/encoder.yml', needs='extractor', timeout_ready=60000, replicas=2
-    ).add(
-        name='compound_chunk_indexer',
-        yaml_path='images/compound_chunk_indexer/compound_chunk_indexer.yml', needs='encoder', timeout_ready=60000
-    ).add(
-        name='ranker', yaml_path='images/ranker/ranker.yml', needs='compound_chunk_indexer'
-    ).add(
-        name='meta_doc_indexer', yaml_path='images/meta_doc_indexer/meta_doc_indexer.yml',
-        needs='ranker'
-    )
+    flow = (Flow().add(name='extractor', yaml_path='images/extractor/extractor.yml')
+        .add(name='encoder', yaml_path='images/encoder/encoder.yml', needs='extractor', timeout_ready=60000, replicas=2)
+        .add(name='compound_chunk_indexer', yaml_path='images/compound_chunk_indexer/compound_chunk_indexer.yml',
+             needs='encoder', timeout_ready=60000)
+        .add(name='ranker', yaml_path='images/ranker/ranker.yml', needs='compound_chunk_indexer')
+        .add(name='meta_doc_indexer', yaml_path='images/meta_doc_indexer/meta_doc_indexer.yml', needs='ranker'))
 
     def print_topk(resp, fp):
         for d in resp.search.docs:
@@ -49,11 +42,13 @@ def main():
             v['metaInfo'] = d.raw_bytes.decode()
             for k, kk in zip(v['topkResults'], d.topk_results):
                 k['matchDoc']['metaInfo'] = kk.match_doc.raw_bytes.decode()
-            fp.write(json.dumps(v, sort_keys=True, indent=4, ensure_ascii=False)+"\n")
+            fp.write(json.dumps(v, sort_keys=True, indent=4, ensure_ascii=False) + "\n")
+
     with open("{}/query_result.json".format(os.environ['TMP_WORKSPACE']), "w") as fp:
         with flow.build() as f:
             pr = lambda x: print_topk(x, fp)
             f.search(raw_bytes=read_data(data_fn), callback=pr, top_k=3)
+
 
 if __name__ == '__main__':
     main()
