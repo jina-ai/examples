e03537c6f742bb3f3f853007cf208232350f3e2e
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-04-20 16:32:38 +0800

    refactor: refactor the main
    Former-commit-id: 73190fb6ef6e8e307b1e60c9356f9dc6661bcdf6 [formerly 0a4a156cf91e6a8bb38fc251a6174b19f0ad4618]
    Former-commit-id: b6c1a1a9ef214b9757a50f5ec8ea75f71f880abc

--- urbandict-search/yaml/customized_executors.py
+++ urbandict-search/yaml/customized_executors.py
@@ -1,35 +1,19 @@
-from jina.executors.crafters import BaseSegmenter
-from jina.executors.encoders import BaseTextEncoder
+from jina.executors.crafters import BaseDocCrafter
 import json
-import numpy as np
 
 
-class DictEntryExtractor(BaseSegmenter):
+class DictEntryExtractor(BaseDocCrafter):
     def craft(self, doc_id, raw_bytes, *args, **kwargs):
-        json_str = raw_bytes.decode("utf8")
+        json_str = raw_bytes.decode('utf8')
         json_dict = json.loads(json_str)
-        word = json_dict["word"]
-        chunk_list = []
-        for idx, d in enumerate(json_dict["def"]):
-            chunk_text = d["text"]
-            chunk_weight = d["weight"]
-            chunk_list.append(
-                {
-                    "raw_bytes": chunk_text.encode("utf8"),
-                    "doc_id": doc_id,
-                    "offset": idx,
-                    "weight": chunk_weight,
-                    "length": len(chunk_text),
-                    "text": chunk_text
-                }
-            )
-        return chunk_list
+        word = json_dict['word']
+        def_text = json_dict['text']
+        weight = json_dict['weight']
+        doc_dict = {
+            'raw_bytes': f'{word}: {def_text}'.encode('utf8'),
+            'doc_id': doc_id,
+            'weight': weight,
+            'meta_info': raw_bytes
+        }
+        return doc_dict
 
-
-class RandomEncoder(BaseTextEncoder):
-    def encode(self, data, *args, **kwargs):
-        num_chunks = data.shape[0]
-        emb_dim = 128
-        emb_result = np.random.rand(num_chunks, emb_dim)
-        self.logger.info("encode random embedding for the sentences")
-        return emb_result
