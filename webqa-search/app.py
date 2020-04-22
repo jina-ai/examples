e4971233842f3effa5550bdb04a1aa6b85302b2e
xiong ma <maxiong@juexiaotime.com>
2020-04-22 14:41:14 +0800

    refactor app

--- webqa-search/app.py
+++ webqa-search/app.py
@@ -2,16 +2,12 @@
 import json
 import os
 
-from google.protobuf.json_format import MessageToDict
+import click
 from jina.flow import Flow
 
 workspace_path = '/tmp/jina/webqa/'
-index_file = 'web_text_zh_valid.json'
-query_file = 'web_text_zh_valid.json'
-do_index = True
 
 os.environ['TMP_WORKSPACE'] = workspace_path
-os.environ['REPLICAS'] = '1'
 
 def read_data(fn):
     items = {}
@@ -23,38 +19,50 @@ def read_data(fn):
             if item['qid'] not in items.keys():
                 items[item['qid']] = {}
                 items[item['qid']]['title'] = item['title']
-                items[item['qid']]['answers'] = [{'answer_id': item['answer_id'], 'content': item['content']}]
+                items[item['qid']]['answers'] = [{'content': item['content']}]
             else:
-                items[item['qid']]['answers'].append({'answer_id': item['answer_id'], 'content': item['content']})
+                items[item['qid']]['answers'].append({'content': item['content']})
 
     result = []
-    for qid, value in items.items():
-        value['qid'] = qid
+    for _, value in items.items():
         result.append(("{}".format(json.dumps(value, ensure_ascii=False))).encode("utf-8"))
 
     for item in result[:100]:
         yield item
 
-def print_topk(resp, fp):
+def print_topk(resp):
+    print(f'以下是相似的问题:')
     for d in resp.search.docs:
-        v = MessageToDict(d, including_default_value_fields=True)
-        v['metaInfo'] = d.raw_bytes.decode()
-        for k, kk in zip(v['topkResults'], d.topk_results):
-            k['matchDoc']['metaInfo'] = kk.match_doc.raw_bytes.decode()
-        fp.write(json.dumps(v, sort_keys=True, indent=4)+"\n")
+        for tk in d.topk_results:
+            item = json.loads(tk.match_doc.raw_bytes.decode('utf-8'))
+            print('→%s' % item['title'])
 
-if do_index:
-    # index
-    f = Flow.load_config('flow_index.yml')
-    data_fn = os.path.join(workspace_path, index_file)
-    with f.build() as fl:
-        fl.index(raw_bytes=read_data(data_fn))
+def read_query_data(item):
+    yield ("{}".format(json.dumps(item, ensure_ascii=False))).encode('utf-8')
 
-else:
-    # query
-    q = Flow.load_config('flow_query.yml')
-    data_fn = os.path.join(workspace_path, query_file)
-    with open("{}/query_result.json".format(os.environ['TMP_WORKSPACE']), "w") as fp:
-        with q.build() as fl:
-            pr = lambda x: print_topk(x, fp)
-            fl.search(raw_bytes=read_data(data_fn), callback=pr)
+@click.command()
+@click.option('--task', '-t', default='query')
+@click.option('--top_k', '-k', default=5)
+def main(task, top_k):
+    if task == 'index':
+        data_fn = os.path.join(workspace_path, "web_text_zh_train.json")
+        flow = Flow().load_config('flow-index.yml')
+        with flow.build() as fl:
+            fl.index(raw_bytes=read_data(data_fn), batch_size=4)
+
+    elif task == 'query':
+        flow = Flow().load_config('flow-query.yml')
+        with flow.build() as fl:
+            while True:
+                title = input('请输入问题: ')
+                item = {'title': title}
+                if not title:
+                    break
+                ppr = lambda x: print_topk(x)
+                fl.search(read_query_data(item), callback=ppr, topk=top_k)
+    else:
+        raise NotImplementedError(
+            f'unknown task: {task}. A valid task is either `index` or `query`.')
+
+if __name__ == '__main__':
+    main()
