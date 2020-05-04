9c723902cc6eb9184fa95aa593326d892edbc20a
xiong ma <maxiong@juexiaotime.com>
2020-05-04 14:00:32 +0800

    fix some contents
    Former-commit-id: d12eb1cf7f0e4842056afee03010f865059b5ae1 [formerly 95dd5320bc047aaa1f574c4a9e045e7f52637790]
    Former-commit-id: b9709b1cda6246470ead8d42c52672121b7301b9

--- webqa-search/prepare_data.py
+++ webqa-search/prepare_data.py
@@ -1,6 +1,6 @@
-
 import os
 import zipfile
+import json
 
 root_path = '/tmp/jina/'
 demo_name = 'webqa'
@@ -14,3 +14,23 @@ fz = zipfile.ZipFile(os.path.join(workspace, 'webtext2019_zh.zip'), 'r')
 
 for file in fz.namelist():
     fz.extract(file, workspace)
+
+for filename in os.listdir(workspace):
+    if not filename.endswith('.json'):
+        continue
+
+    items = {}
+    with open(os.path.join(workspace, filename), 'r', encoding='utf-8') as f:
+        for line in f:
+            item = json.loads(line)
+            if item['content'] == '':
+                continue
+            if item['qid'] not in items.keys():
+                items[item['qid']] = {}
+                items[item['qid']]['title'] = item['title']
+                items[item['qid']]['answers'] = [{'content': item['content']}]
+            else:
+                items[item['qid']]['answers'].append({'content': item['content']})
+
+    with open(os.path.join(workspace, 'pre_%s' % filename), 'w', encoding='utf-8') as writer:
+        json.dump(items, writer, ensure_ascii=False, indent=4)
