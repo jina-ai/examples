fee4c7a39f2e29408f28e4d06a38eab220c85af6
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-05-09 21:41:19 +0800

    fix: add the option for toy running

--- webqa-search/app.py
+++ webqa-search/app.py
@@ -1,6 +1,6 @@
-
 import json
 import os
+import random
 
 import click
 from jina.flow import Flow
@@ -9,16 +9,20 @@ workspace_path = '/tmp/jina/webqa/'
 
 os.environ['TMP_WORKSPACE'] = workspace_path
 
-def read_data(fn):
+
+def read_data(fn, num_docs):
     with open(os.path.join(workspace_path, fn), 'r', encoding='utf-8') as f:
         items = json.load(f)
-
     result = []
+    random.seed(0)
     for _, value in items.items():
         result.append(("{}".format(json.dumps(value, ensure_ascii=False))).encode("utf-8"))
-
+    if num_docs > 0:
+        random.shuffle(result)
+        result = result[:num_docs]
     for item in result:
         yield item
+
 
 def print_topk(resp):
     print(f'以下是相似的问题:')
@@ -27,18 +31,21 @@ def print_topk(resp):
             item = json.loads(tk.match_doc.raw_bytes.decode('utf-8'))
             print('👉%s' % item['title'])
 
+
 def read_query_data(item):
     yield ("{}".format(json.dumps(item, ensure_ascii=False))).encode('utf-8')
+
 
 @click.command()
 @click.option('--task', '-t', default='query')
 @click.option('--top_k', '-k', default=5)
-def main(task, top_k):
+@click.option('--num_docs', '-n', default=10)
+def main(task, top_k, num_docs):
     if task == 'index':
         data_fn = os.path.join(workspace_path, "pre_web_text_zh_train.json")
         flow = Flow().load_config('flow-index.yml')
         with flow:
-            flow.index(read_data(data_fn), batch_size=32)
+            flow.index(read_data(data_fn, num_docs), batch_size=32)
 
     elif task == 'query':
         flow = Flow().load_config('flow-query.yml')
@@ -53,6 +60,7 @@ def main(task, top_k):
     else:
         raise NotImplementedError(
             f'unknown task: {task}. A valid task is either `index` or `query`.')
+
 
 if __name__ == '__main__':
     main()
