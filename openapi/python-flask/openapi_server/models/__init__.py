5db5bd6aaa24bd7d90a84f0cac432b16683a2e58
yangboz <youngwelle@gmail.com>
2020-05-15 14:04:08 +0800

    ADDED:openapi example include spec file and generated python-flask project files,tox unittest passed;

---
+++ openapi/python-flask/openapi_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.api_response import ApiResponse
+from openapi_server.models.doc import Doc
