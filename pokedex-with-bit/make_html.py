b7d87951a583fc1f790a4e878edc4de45d382d4e
Han Xiao <han.xiao@jina.ai>
2020-06-01 15:37:47 +0200

    fix: update to new ingest api

--- pokedex-with-bit/make_html.py
+++ pokedex-with-bit/make_html.py
@@ -1,12 +1,9 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-import glob
 import os
-import random
 import webbrowser
 
-from jina.enums import ClientInputType
 from jina.flow import Flow
 from pkg_resources import resource_filename
 
@@ -14,7 +11,6 @@ from pkg_resources import resource_filename
 image_src = 'data/**/*.png'
 replicas = 1
 shards = 8
-sampling_rate = .99
 
 os.environ['REPLICAS'] = str(replicas)
 os.environ['SHARDS'] = str(shards)
@@ -23,13 +19,6 @@ os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
 
 f = Flow.load_config('flow-query.yml')
 f.use_grpc_gateway()  # use gRPC gateway for better batch efficiency
-
-
-def input_fn():
-    for g in glob.glob(image_src, recursive=True):
-        if random.random() > sampling_rate:
-            yield g
-
 
 result_html = []
 
@@ -61,7 +50,6 @@ def write_html(html_path):
 
 
 with f:
-    f.search(input_fn, batch_size=8, output_fn=print_result,
-             top_k=20, input_type=ClientInputType.DATA_URI)
+    f.search_files(image_src, sampling_rate=.01, batch_size=8, output_fn=print_result, top_k=20)
 
 write_html('result.html')
