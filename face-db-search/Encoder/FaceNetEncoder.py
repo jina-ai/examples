540e67c94e791a3b7cc9e407102677b06faee088
Shivam Raj <connect2shivamraj@gmail.com>
2020-06-06 12:15:36 +0530

    FaceNet Docker Update
    Former-commit-id: e24a2a43ba886a6feceb195e559b8bf533e1c22f [formerly a887c396d7044f07326944cfe61b65cb7bf3caed]
    Former-commit-id: 5900c7573c8a7d4d8b269eeb0a7b4183e2690903

---
+++ face-db-search/Encoder/FaceNetEncoder.py
@@ -0,0 +1,31 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+
+from jina.executors.encoders.frameworks import BaseCVTorchEncoder
+
+class FaceNetTorchEncoder(BaseCVTorchEncoder):
+    """
+    :class:`FaceNetTorchEncoder` encodes data from a ndarray, potentially B x (Channel x Height x Width) into a
+        ndarray of `B x D`.
+
+    """
+
+    def __init__(self,pretrained: str='vggface2',pool_strategy: str = 'mean', *args, **kwargs):
+        """
+        :pretrained : The weights to load
+        """
+        super().__init__(*args, **kwargs)
+        if pretrained not in ('vggface2','casia-webface'):
+            raise NotImplementedError('unknown pretrained checkpoints: {}'.format(self.pretrained))
+        self.pretrained = pretrained
+
+    def post_init(self):
+        from facenet_pytorch import InceptionResnetV1
+        self.model= InceptionResnetV1(pretrained=self.pretrained).eval()
+        self.to_device(self.model)
+
+    def _get_features(self, data):
+        return self.model(data)
+
+
