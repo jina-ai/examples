e1d03be1244061d3dda2c3c0cda8756b1b68bd52
yangboz <youngwelle@gmail.com>
2020-06-08 19:46:22 +0800

    CHANGED:added missing pillow of tumblr-gif-search,Dockerfile with  minor changes made

--- tumblr-gif-search/app.py
+++ tumblr-gif-search/app.py
@@ -1,77 +1,65 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-import json
 import os
-from datetime import datetime
-
-from google.protobuf.json_format import MessageToDict
-
-os.environ['JINA_LOG_PROFILING'] = 'true'
+import sys
 
 from jina.flow import Flow
 
-RUN_MODE = 'index'
-MODEL_ID = '20200424191102'
-
-WORK_DIR = '/Volumes/TOSHIBA-4T/model/'
-GIF_BLOB = '/Volumes/TOSHIBA-4T/dataset/thumblr-gif-data/*.gif'  # 'data/*.gif'
-TIMESTAMP = datetime.now().strftime('%Y%m%d%H%M%S')
-
-os.environ['TEST_WORKDIR'] = WORK_DIR + MODEL_ID
-# os.environ['JINA_LOG_FILE'] = 'JSON'
-os.environ['JINA_LOG_PROFILING'] = 'true'
-# os.environ['JINA_LOG_VERBOSITY'] = 'DEBUG'
-# os.environ['GRPC_VERBOSITY'] = 'debug'
-# os.environ['GRPC_TRACE']='tcp'
-do_index = True
-
-shards = 8
-
-if RUN_MODE == 'debug-index':
-    replicas = 2
-    num_docs = 10000
-elif RUN_MODE == 'index':
-    replicas = 3
-    num_docs = 200000
-elif RUN_MODE == 'debug-query' or RUN_MODE == 'query':
-    do_index = False
-    replicas = 1
-    num_docs = 100
-else:
-    raise AttributeError(RUN_MODE)
-
-os.environ['REPLICAS'] = str(replicas)
-os.environ['SHARDS'] = str(shards)
-
-if RUN_MODE.endswith('index'):
-    os.environ['TEST_WORKDIR'] = WORK_DIR + TIMESTAMP
-    os.makedirs(os.environ['TEST_WORKDIR'], exist_ok=True)
+num_docs = os.environ.get('MAX_DOCS', 50000)
+image_src = 'data/**/*.png'
 
 
-def print_result(resp, fp):
-    for d in resp.search.docs:
-        v = MessageToDict(d, including_default_value_fields=True)
-        v['metaInfo'] = d.meta_info.decode()
-        for k, kk in zip(v['topkResults'], d.topk_results):
-            k['matchDoc']['metaInfo'] = kk.match_doc.meta_info.decode()
-            # k['score']['explained'] = json.loads(kk.score.explained)
-        fp.write(json.dumps(v, sort_keys=True) + '\n')
+def config():
+    replicas = 2 if sys.argv[1] == 'index' else 1
+    shards = 8
+
+    os.environ['REPLICAS'] = str(replicas)
+    os.environ['SHARDS'] = str(shards)
+    os.environ['WORKDIR'] = './workspace'
+    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
 
 
-if do_index:
-    # index
+# for index
+def index():
+    # from jina.clients.python import PyClient
+    # PyClient.check_input(input_fn())
+
     f = Flow.load_config('flow-index.yml')
 
-    # input_fn = (g.encode() for g in glob.glob(GIF_BLOB)[:num_docs])
+    with f:
+        f.index_files(image_src, batch_size=64, read_mode='rb', size=num_docs)
+
+
+# for search
+def search():
+    f = Flow.load_config('flow-query.yml')
 
     with f:
-        f.index_files(GIF_BLOB, batch_size=8, size=num_docs, read_mode='rb', mime_type='gif')
-else:
-    # query
-    q = Flow.load_config('flow-query.yml')
+        f.block()
 
-    with open(os.environ['TEST_WORKDIR'] + '/topk.json', 'w', encoding='utf8') as fp:
-        ppr = lambda x: print_result(x, fp)
-        with q:
-            q.search_files(GIF_BLOB, size=num_docs, shuffle=True, callback=ppr, top_k=80, batch_size=8)
+
+# for test before put into docker
+def dryrun():
+    f = Flow.load_config('flow-query.yml')
+
+    with f:
+        pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('choose between "index" and "search" mode')
+        exit(1)
+    if sys.argv[1] == 'index':
+        config()
+        index()
+    elif sys.argv[1] == 'search':
+        config()
+        search()
+    elif sys.argv[1] == 'dryrun':
+        config()
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
