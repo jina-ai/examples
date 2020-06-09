0015eb2aab5dc5dc0f9d2bc61e1fbae90fdec5b7
Xiong Ma <xiong.ma@jina.ai>
2020-06-09 18:27:25 +0800

    perfect: update to latest api
    Former-commit-id: f8419a5b140fbe7ab7b6cd9faaa4ae78ed0bf55f [formerly f86aa8d9663b747196995d0beda550a5e402885e]
    Former-commit-id: d70af05dc8b8e25af115236c4f80d0e4dcd84c0c

---
+++ zh/webqa-search/app.py
@@ -0,0 +1,55 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import json
+import os
+import random
+
+import click
+from jina.flow import Flow
+
+workspace_path = '/tmp/jina/webqa/'
+
+os.environ['TMP_WORKSPACE'] = workspace_path
+
+
+def print_topk(resp):
+    print(f'以下是相似的问题:')
+    for d in resp.search.docs:
+        for tk in d.topk_results:
+            item = json.loads(tk.match_doc.text)
+            print('👉%s' % item['title'])
+
+
+def read_query_data(item):
+    yield ("{}".format(json.dumps(item, ensure_ascii=False)))
+
+
+@click.command()
+@click.option('--task', '-t', default='query')
+@click.option('--top_k', '-k', default=5)
+@click.option('--num_docs', '-n', default=100)
+def main(task, top_k, num_docs):
+    if task == 'index':
+        data_fn = os.path.join(workspace_path, "pre_web_text_zh_valid.json")
+        flow = Flow().load_config('flow-index.yml')
+        with flow:
+            flow.index_lines(filepath=data_fn, size=num_docs, batch_size=32)
+
+    elif task == 'query':
+        flow = Flow().load_config('flow-query.yml')
+        with flow:
+            while True:
+                title = input('请输入问题: ')
+                item = {'title': title}
+                if not title:
+                    break
+                ppr = lambda x: print_topk(x)
+                flow.search(read_query_data(item), output_fn=ppr, topk=top_k)
+    else:
+        raise NotImplementedError(
+            f'unknown task: {task}. A valid task is either `index` or `query`.')
+
+
+if __name__ == '__main__':
+    main()
