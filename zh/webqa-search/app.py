f86aa8d9663b747196995d0beda550a5e402885e
Xiong Ma <xiong.ma@jina.ai>
2020-06-09 18:27:25 +0800

    perfect: update to latest api

--- zh/webqa-search/app.py
+++ zh/webqa-search/app.py
@@ -13,42 +13,28 @@ workspace_path = '/tmp/jina/webqa/'
 os.environ['TMP_WORKSPACE'] = workspace_path
 
 
-def read_data(fn, num_docs):
-    with open(os.path.join(workspace_path, fn), 'r', encoding='utf-8') as f:
-        items = json.load(f)
-    result = []
-    random.seed(0)
-    for _, value in items.items():
-        result.append(("{}".format(json.dumps(value, ensure_ascii=False))).encode("utf-8"))
-    if num_docs > 0:
-        random.shuffle(result)
-        result = result[:num_docs]
-    for item in result:
-        yield item
-
-
 def print_topk(resp):
     print(f'以下是相似的问题:')
     for d in resp.search.docs:
         for tk in d.topk_results:
-            item = json.loads(tk.match_doc.buffer.decode('utf-8'))
+            item = json.loads(tk.match_doc.text)
             print('👉%s' % item['title'])
 
 
 def read_query_data(item):
-    yield ("{}".format(json.dumps(item, ensure_ascii=False))).encode('utf-8')
+    yield ("{}".format(json.dumps(item, ensure_ascii=False)))
 
 
 @click.command()
-@click.option('--task', '-t', default='index')
+@click.option('--task', '-t', default='query')
 @click.option('--top_k', '-k', default=5)
-@click.option('--num_docs', '-n', default=10)
+@click.option('--num_docs', '-n', default=100)
 def main(task, top_k, num_docs):
     if task == 'index':
         data_fn = os.path.join(workspace_path, "pre_web_text_zh_valid.json")
         flow = Flow().load_config('flow-index.yml')
         with flow:
-            flow.index_lines(lines=read_data(data_fn, num_docs), batch_size=32)
+            flow.index_lines(filepath=data_fn, size=num_docs, batch_size=32)
 
     elif task == 'query':
         flow = Flow().load_config('flow-query.yml')
