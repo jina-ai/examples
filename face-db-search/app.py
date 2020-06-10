36b8656ec73c9d4f19ec40e121145403348fee49
Shivam Raj <connect2shivamraj@gmail.com>
2020-06-10 14:13:00 +0530

    Update to latest API
    Former-commit-id: aa5658b18056befc5ef5dd96f22cb3fede35cdc6 [formerly 30a05c921262e6ac2c656d37ea97c75525859274]
    Former-commit-id: 7ef19ac7fd942bec74a600b7411bedff080d6c5e

---
+++ face-db-search/app.py
@@ -0,0 +1,65 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+
+import os
+import sys
+
+from jina.flow import Flow
+
+num_docs = os.environ.get('MAX_DOCS', 500)
+image_src = '/tmp/jina/celeb/lfw/**/*.jpg'
+
+
+def config():
+    replicas = 2 if sys.argv[1] == 'index' else 1
+    shards = 8
+
+    os.environ['TMP_WORKSPACE'] = '/tmp/jina/workspace'
+    os.environ['COLOR_CHANNEL_AXIS'] = str(0)
+    os.environ['REPLICAS'] = str(replicas)
+    os.environ['SHARDS'] = str(shards)
+    os.environ['WORKDIR'] = '/tmp/jina/workspace'
+    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
+
+
+# for index
+def index():
+    f = Flow.load_config('flow-index.yml')
+
+    with f:
+        f.index_files(image_src, batch_size=8, read_mode='rb', size=num_docs)
+
+
+# for search
+def search():
+    f = Flow.load_config('flow-query.yml')
+
+    with f:
+        f.block()
+
+
+# for test before put into docker
+def dryrun():
+    f = Flow.load_config('flow-query.yml')
+
+    with f:
+        pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('choose between "index" and "search" mode')
+        exit(1)
+    if sys.argv[1] == 'index':
+        config()
+        index()
+    elif sys.argv[1] == 'search':
+        config()
+        search()
+    elif sys.argv[1] == 'dryrun':
+        config()
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
