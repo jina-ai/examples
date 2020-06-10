5ad3dfe16d79bdd7ec1944dacf0e7051b6db0a3b
Shivam Raj <connect2shivamraj@gmail.com>
2020-06-10 14:13:00 +0530

    Update to latest API

--- face-db-search/app.py
+++ face-db-search/app.py
@@ -2,141 +2,64 @@ __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 
-import click
 import os
-import string
-import random
 import sys
 
 from jina.flow import Flow
 
-RANDOM_SEED = 42
-os.environ['REPLICAS'] = str(1)
-os.environ['SHARDS'] = str(1)
-os.environ['TMP_DATA_DIR'] = '/tmp/jina/celeb'
-os.environ['COLOR_CHANNEL_AXIS'] = str(0)
+num_docs = os.environ.get('MAX_DOCS', 500)
+image_src = '/tmp/jina/celeb/lfw/**/*.jpg'
 
 
-def get_random_ws(workspace_path, length=8):
-    random.seed(RANDOM_SEED)
-    letters = string.ascii_lowercase
-    dn = ''.join(random.choice(letters) for i in range(length))
-    return os.path.join(workspace_path, dn)
+def config():
+    replicas = 2 if sys.argv[1] == 'index' else 1
+    shards = 8
 
-def read_custom_data(img_path, max_sample_size=-1):
-    if not os.path.exists(img_path):
-        raise FileNotFoundError('file not found: {}'.format(img_path))
-
-    fn_list = []
-    if os.path.isdir(img_path):
-        path_join_fn = lambda d, f: os.path.join(d, f)
-        fn_list = [ path_join_fn(img_path, f) for f in os.listdir(img_path) if os.path.isfile(path_join_fn(img_path, f)) and f.endswith('.jpg') ]
-    elif os.path.isfile(img_path):
-        if img_path.endswith('.jpg'):
-            fn_list.append(img_path)
-    else:
-        raise FileNotFoundError('{} must be a file or directory.'.format(img_path));
-
-    if not fn_list:
-        raise FileNotFoundError('No .jpg file or files to query, path: {}'.format(img_path));
-
-    if max_sample_size > 0:
-        random.shuffle(fn_list)
-        fn_list = fn_list[:max_sample_size]
-
-    for fn in fn_list:
-        yield fn.encode('utf8')
-
-def read_data(img_path, max_sample_size=-1):
-    if not os.path.exists(img_path):
-        raise FileNotFoundError('file not found: {}'.format(img_path))
-    fn_list = []
-    for dirs, subdirs, files in os.walk(img_path):
-        for f in files:
-            fn = os.path.join(dirs, f)
-            if fn.endswith('.jpg'):
-                fn_list.append(fn)
-    if max_sample_size > 0:
-        random.shuffle(fn_list)
-        fn_list = fn_list[:max_sample_size]
-    for fn in fn_list:
-        yield fn.encode('utf8')
+    os.environ['TMP_WORKSPACE'] = '/tmp/jina/workspace'
+    os.environ['COLOR_CHANNEL_AXIS'] = str(0)
+    os.environ['REPLICAS'] = str(replicas)
+    os.environ['SHARDS'] = str(shards)
+    os.environ['WORKDIR'] = '/tmp/jina/workspace'
+    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
 
 
-def save_topk(resp, output_fn=None):
-    results = []
-    for d in resp.search.docs:
-        cur_result = []
-        d_fn = d.meta_info.decode()
-        cur_result.append(d_fn)
-        print("-" * 20)
-        print(f'query image: {d_fn}')
-        print('matched images' + "*" * 10)
-        print("{}".format(d.buffer.decode()))
-        for idx, kk in enumerate(d.topk_results):
-            score = kk.score.value
-            if score < 0.0:
-                continue
-            m_fn = kk.match_doc.buffer.decode()
-            print('{:>2d}:({:f}):{}'.format(
-                idx, score, m_fn))
-            cur_result.append(m_fn)
-        results.append(cur_result)
-    if output_fn is not None:
-        import matplotlib.pyplot as plt
-        import matplotlib.image as mpimg
-        top_k = max([len(r) for r in results])
-        num_q = len(resp.search.docs)
-        f, ax = plt.subplots(num_q, top_k, figsize=(8, 20), squeeze=False)
-        for q_idx, r in enumerate(results):
-            for m_idx, img in enumerate(r):
-                fname = os.path.split(img)[-1]
-                names=fname.split('_')[:-1]
-                fname=''.join(names)
-                fname = f'' if m_idx == 0 else f'top_{m_idx}: {fname}'
-                ax[q_idx][m_idx].imshow(mpimg.imread(img))
-                ax[q_idx][m_idx].set_title(fname, fontsize=7)
-        [aa.axis('off') for a in ax for aa in a]
-        plt.tight_layout()
-        plt.savefig(output_fn)
+# for index
+def index():
+    f = Flow.load_config('flow-index.yml')
+
+    with f:
+        f.index_files(image_src, batch_size=8, read_mode='rb', size=num_docs)
 
 
-@click.command()
-@click.option('--task', '-t')
-@click.option('--num_docs', '-n', default=500)
-@click.option('--top_k', '-k', default=5)
-@click.option('--path', '-p', help='Specify a JPG file or directory to query', default='')
-def main(task, num_docs, top_k, path):
-    from jina.clients.python import PyClient
-    os.environ['TMP_WORKSPACE'] = get_random_ws(os.environ['TMP_DATA_DIR'])
-    data_path = os.path.join(os.environ['TMP_DATA_DIR'], 'lfw')
-    if task == 'index':
-        flow = Flow().load_config('flow-index.yml')
-        with flow:
-            flow.index_file(...)
-            print('Checking Input...')
-            PyClient.check_input(read_data(data_path, num_docs))
-            print('Start Indexing...')
-            fl.index(buffer=read_data(data_path, num_docs), batch_size=4)
-    elif task == 'query':
-        if not path:
-            cmd_prompt = '\033[{}mpython {} -t query -p <JPG file or directory>\033[0m'.format(32, sys.argv[0])
-            prompt = input(f'You can specify a JPG file or directory you own to query: {cmd_prompt}\nDo you want? Please input y or n: ')
-            if prompt and prompt == 'y':
-               sys.exit(0)
+# for search
+def search():
+    f = Flow.load_config('flow-query.yml')
 
-        read_data_fn = read_custom_data if path else read_data
-        if path:
-            data_path = path
+    with f:
+        f.block()
 
-        flow = Flow().load_config('flow-query.yml')
-        with flow:
-            ppr = lambda x: save_topk(x, os.path.join(os.environ['TMP_DATA_DIR'], 'query_results.png'))
-            fl.search_files(...)
-    else:
-        raise NotImplementedError(
-            f'unknown task: {task}. A valid task is either `index` or `query`.')
+
+# for test before put into docker
+def dryrun():
+    f = Flow.load_config('flow-query.yml')
+
+    with f:
+        pass
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        print('choose between "index" and "search" mode')
+        exit(1)
+    if sys.argv[1] == 'index':
+        config()
+        index()
+    elif sys.argv[1] == 'search':
+        config()
+        search()
+    elif sys.argv[1] == 'dryrun':
+        config()
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
