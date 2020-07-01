c7db6795837378e4eb05c5805e1b7f1012fbd7c9
YueLiu-jina <yue.liu@jina.ai>
2020-07-01 13:25:03 +0800

    fix(urbandict): fix problem

--- urbandict-search/app.py
+++ urbandict-search/app.py
@@ -47,7 +47,8 @@ def main(task, num_docs, top_k):
     workspace_path = '/tmp/jina/urbandict'
     os.environ['TMP_WORKSPACE'] = get_random_ws(workspace_path)
     print(f'{os.environ["TMP_WORKSPACE"]}')
-    data_fn = os.path.join('/tmp/jina/urbandict', "urbandict-word-defs.csv")
+    data_dir = os.path.join(os.environ.get('DATA_DIR', '/tmp'),'jina','urbandict')
+    data_fn = os.path.join(data_dir, "urbandict-word-defs.csv")
     if task == 'index':
         f = Flow().load_config('flow-index.yml')
         with f:
