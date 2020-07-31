3d5f9d1f5b12ed1f84ea6999506275f690bce95e
YueLiu-jina <yue.liu@jina.ai>
2020-07-31 10:15:32 +0800

    fix(flower): refactor

--- flower-search/app.py
+++ flower-search/app.py
@@ -12,14 +12,16 @@ from PIL import Image
 from jina.flow import Flow
 
 RANDOM_SEED = 14
-os.environ['PARALLEL'] = str(1)
-os.environ['SHARDS'] = str(2)
-os.environ['TMP_DATA_DIR'] = '/tmp/jina/flower'
-os.environ['COLOR_CHANNEL_AXIS'] = str(0)
-os.environ['JINA_PORT'] = str(45678)
-# os.environ['ENCODER'] = os.environ.get('ENCODER', 'jinaai/hub.executors.encoders.image.torchvision-mobilenet_v2')
-os.environ['ENCODER'] = 'yaml/encode.yml'
-os.environ['TMP_WORKSPACE'] = os.environ.get('TMP_WORKSPACE', get_random_ws(os.environ['TMP_DATA_DIR']))
+
+def config():
+    os.environ['PARALLEL'] = str(1)
+    os.environ['SHARDS'] = str(2)
+    os.environ['TMP_DATA_DIR'] = '/tmp/jina/flower'
+    os.environ['COLOR_CHANNEL_AXIS'] = str(0)
+    os.environ['JINA_PORT'] = str(45678)
+    # os.environ['ENCODER'] = os.environ.get('ENCODER', 'jinaai/hub.executors.encoders.image.torchvision-mobilenet_v2')
+    os.environ['ENCODER'] = 'yaml/encode.yml'
+    os.environ['TMP_WORKSPACE'] = os.environ.get('TMP_WORKSPACE', get_random_ws(os.environ['TMP_DATA_DIR']))
 
 def get_random_ws(workspace_path, length=8):
     random.seed(RANDOM_SEED)
@@ -32,6 +34,7 @@ def get_random_ws(workspace_path, length=8):
 @click.option('--task', '-t')
 @click.option('--num_docs', '-n', default=50)
 def main(task, num_docs):
+    config()
     data_path = os.path.join(os.environ['TMP_DATA_DIR'], 'jpg')
     if task == 'index':
         f = Flow().load_config('flow-index.yml')
