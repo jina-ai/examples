5baafeca08a22fd8961afa96a3165c7edde9130c
YueLiu-jina <yue.liu@jina.ai>
2020-08-03 13:53:22 +0800

    fix(helloworldCS): migrate
    Former-commit-id: 69392ef79d5d8be5c7e65d7725d7131db6203945 [formerly 32cd49d73f665e878b293c58cc5d4f5aed9fb02c]
    Former-commit-id: 6a9c6b4a90695ec12e778874fb43558ac6756e2e

--- helloworld-in-cs/client.py
+++ helloworld-in-cs/client.py
@@ -2,12 +2,25 @@ __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import os
+import numpy as np
 from pathlib import Path
 
 from jina.clients import py_client
-from jina.helloworld.helper import input_fn, download_data
+from jina.helloworld.helper import load_mnist, download_data
 from jina.main.parser import set_hw_parser
 
+
+def input_fn(fp, index=True, num_doc=None):
+    img_data = load_mnist(fp)
+    if not index:
+        # shuffle for random query
+        img_data = np.take(img_data, np.random.permutation(img_data.shape[0]), axis=0)
+    d_id = 0
+    for r in img_data:
+        yield r.tobytes()
+        d_id += 1
+        if num_doc is not None and d_id > num_doc:
+            break
 
 def hello_world(args):
     Path(args.workdir).mkdir(parents=True, exist_ok=True)
