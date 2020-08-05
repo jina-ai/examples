a9fb36007a291b4f09c23596888ba2cbfc967ae4
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-08-05 11:20:07 +0200

    fix: fix faiss search
    Former-commit-id: 94ce08bc285bdd5fb719ceec9ad689c7ad02d90f [formerly 6f263a92bf1df81e438159ee6586c08b582eab95]
    Former-commit-id: 64cda7c83788250189103d6627fe638e283698af

--- faiss-search/yaml/custom_executors.py
+++ faiss-search/yaml/custom_executors.py
@@ -1,14 +1,7 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from jina.executors.crafters import BaseSegmenter
 from jina.executors.encoders import BaseNumericEncoder
-
-
-class MySegmenter(BaseSegmenter):
-
-    def craft(self, blob, *args, **kwargs):
-        return [dict(blob=blob)]
 
 
 class MyEncoder(BaseNumericEncoder):
