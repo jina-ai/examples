ce1edd9931a593ceb76c4b48a19c7fbee4cbe9e4
Yue Liu <yue.liu@jina.ai>
2020-08-10 14:37:01 +0800

    fix(southpark): change pooling strategy and fix bug
    Former-commit-id: 97830dfd6156530a2d6ca66cbd3ed718edfb8ea0 [formerly 39fb7bb2a1bf66168d12474ae21bcac0a4b31e63]
    Former-commit-id: b25de9bde04d148f74784fa9a190b02b11fe1d3b

--- southpark-search/pods/text_loader.py
+++ southpark-search/pods/text_loader.py
@@ -9,4 +9,4 @@ from jina.executors.crafters import BaseCrafter
 class TextExtractor(BaseCrafter):
     def craft(self, text: str, *args, **kwargs) -> Dict:
         *_, s = text.split('[SEP]')
-        return dict(weight=1., text=s, meta_info=s.encode('utf8'))
+        return dict(weight=1., text=s, meta_info=text.encode('utf8'))
