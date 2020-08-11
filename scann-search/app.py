871ae12006ef20f09223f501df89ffd479e21658
Susana Guzman <zuxi.w13@gmail.com>
2020-08-11 16:04:10 +0200

    feat(examples): scann search

--- scann-search/app.py
+++ scann-search/app.py
@@ -11,4 +11,17 @@ import scann
 
 from jina.flow import Flow
 
+os.environ['TMP_DATA_DIR'] = '/tmp/jina/scann'
+
+@click.command()
+def main(task):
+    if task == 'index':
+        data_path = os.path.join(os.environ['TMP_DATA_DIR'], 'glove_angular.hdf5')
+        flow = Flow().load_config('flow-index.yml')
+    elif task == 'query':
+        data_path = os.path.join(os.environ['TMP_DATA_DIR'], 'glove_angular.hdf5')
+        flow = Flow().load_config('flow-query.yml')
+    else:
+        raise NotImplementedError(
+            f'unknown task: {task}. A valid task is either `index` or `query`.')
 
