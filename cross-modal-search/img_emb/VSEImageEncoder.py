6962cefa54dbba7f70a4ffd8d81048ab44ab558b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-08-14 11:55:05 +0200

    remove transformers from img encoders, they should be handled in previous pod
    Former-commit-id: 53dd04b27d67fafed205caca88be5c9c1b5953b1 [formerly b3cd2dbece82fa03312f5960b4a34f8e705e481b]
    Former-commit-id: 924cf2654b770b29cd8e645fb61baa0a419fde48

---
+++ cross-modal-search/img_emb/VSEImageEncoder.py
@@ -0,0 +1,43 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import torch
+from torch.autograd import Variable
+# model is a file from the vsepp github
+from model import VSE
+
+from jina.executors.encoders.image.torchvision import ImageTorchEncoder
+
+
+class VSEImageEncoder(ImageTorchEncoder):
+    """
+    :class:`VSEImageEncoder` encodes data from a ndarray, potentially B x (Channel x Height x Width) into a
+        ndarray of `B x D`. using VSE img_emb branch
+
+    """
+
+    def __init__(self, path: str = 'runs/f30k_vse++_vggfull/model_best.pth.tar', *args, **kwargs):
+        """
+        :path : path where to find the model.pth file
+        """
+        super().__init__(*args, **kwargs)
+        self.path = path
+
+    def post_init(self):
+
+        checkpoint = torch.load(self.path,
+                                map_location=torch.device('cpu' if not self.on_gpu else 'cuda'))
+        opt = checkpoint['opt']
+
+        model = VSE(opt)
+        model.load_state_dict(checkpoint['model'])
+        model.img_enc.eval()
+        self.model = model.img_enc
+        del model.txt_enc
+
+    def _get_features(self, data):
+        # It needs Resize and Normalization before reaching this Point in another Pod
+        image = Variable(data, requires_grad=False)
+        image = image.unsqueeze(0)
+        img_emb = self.model(image)
+        return img_emb
