298c652360bdadba9052b50d4cb2ec93d81abb7a
JoanFM <joan.martinez@jina.ai>
2020-08-19 13:01:43 +0200

    clean dockerfiles
    Former-commit-id: b5e966ad3f15b08d612a444d7f06aa85d5ae2b87 [formerly abdec62a97db4ef6a1d6e898cd689acb1ad73459]
    Former-commit-id: 948119578ef89b3d34571732df840f5564ad58fa

--- cross-modal-search/img_emb/install.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/JoanFM/vsepp.git -b works
-mv vsepp/model.py .
-rm -rf vsepp
--- cross-modal-search/txt_emb/install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/JoanFM/vsepp.git -b works
-mv vsepp/model.py .
-mv vsepp/vocab.py .
-rm -rf vsepp
