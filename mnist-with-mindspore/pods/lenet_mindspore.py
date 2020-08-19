8b625abc9cb3c49ffd2901f967a4131ea0938cc6
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2020-08-19 20:14:10 +0800

    feat: fix the flow
    Former-commit-id: e5da0165c093e8d7c274c102c4eb0c9c8213080c [formerly c488f1d571d86ee45940ddde1fed903ab8400144]
    Former-commit-id: b8df1d063f69b31d3073e1b3cf11fb8fe2b952e1

--- mnist-with-mindspore/pods/lenet_mindspore.py
+++ mnist-with-mindspore/pods/lenet_mindspore.py
@@ -1,5 +1,6 @@
 from jina.executors.encoders.frameworks import BaseMindsporeEncoder
 from jina.executors.crafters.image.io import ImageReader
+from jina.executors.decorators import batching
 
 import sys
 sys.path.append('..')
@@ -10,9 +11,11 @@ from lenet import *
 
 
 class LeNetImageEncoder(BaseMindsporeEncoder):
+
+    @batching
     def encode(self, data, *args, **kwargs):
         from mindspore import Tensor
-        return self.model(Tensor(data.astype('float32'))).asnumpy()
+        return self.model(Tensor(data[:, 0:1, :, :].astype('float32'))).asnumpy()
 
     def get_model(self):
         return LeNet5()
@@ -21,5 +24,5 @@ class LeNetImageEncoder(BaseMindsporeEncoder):
 class MnistImageReader(ImageReader):
     def craft(self, blob, *args, **kwargs):
         # convert buffer to blob
-        return dict(weight=1., blob=np.stack(blob.reshape(28, 28)))
+        return dict(weight=1., blob=np.stack([blob.reshape(28, 28) for _ in range(3)]))
 
