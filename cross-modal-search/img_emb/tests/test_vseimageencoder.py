287a6b4b1b1c8136655047407292c66fd41ae268
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-08-21 12:27:26 +0200

    add tests and extra files to img_emb

---
+++ cross-modal-search/img_emb/tests/test_vseimageencoder.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+from torchvision import transforms
+from .. import VSEImageEncoder
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_man_piercing_embedding():
+    man_piercing_image_path = os.path.join(cur_dir, 'imgs/man_piercing.jpg')
+    img = Image.open(man_piercing_image_path).convert('RGB')
+
+    transformer = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    image = transformer(img)
+    encoder = VSEImageEncoder()
+
+    embedding = encoder.encode(image.unsqueeze(0).numpy())
+    import numpy as np
+    expected = np.load(os.path.join(cur_dir, 'expected.npy'))
+    assert embedding.shape == (1, 1024)
+    np.testing.assert_almost_equal(embedding, expected)
