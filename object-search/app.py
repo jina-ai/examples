dfc8358e2260f69d0d949089d3757c0bfdfa1b18
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-09-24 22:54:38 +0530

    feat: add object search example

---
+++ object-search/app.py
@@ -0,0 +1,56 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import os
+import shutil
+import sys
+
+import click
+from jina.flow import Flow
+from jina.logging import default_logger as logger
+
+
+num_docs = os.environ.get('MAX_DOCS', 16)
+data_path = 'data/**/*.jpg'
+batch_size = 16
+overwrite_workspace = True
+
+def clean_workdir():
+    if overwrite_workspace and os.path.exists(os.environ['WORKDIR']):
+        shutil.rmtree(os.environ['WORKDIR'])
+        logger.warning('Workspace deleted')
+
+
+def config():
+    parallel = 1 if sys.argv[1] == 'index' else 1
+    shards = 1
+
+    os.environ['PARALLEL'] = str(parallel)
+    os.environ['SHARDS'] = str(shards)
+    os.environ['WORKDIR'] = './workspace'
+    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
+
+
+@click.command()
+@click.option('--task', '-t')
+@click.option('--data_path', '-p', default=data_path)
+@click.option('--num_docs', '-n', default=num_docs)
+@click.option('--batch_size', '-b', default=batch_size)
+def main(task, data_path, num_docs, batch_size):
+    config()
+    if task == 'index':
+        clean_workdir()
+        f = Flow.load_config('flow-index.yml')
+        with f:
+            f.index_files(data_path, batch_size=batch_size, read_mode='rb', size=num_docs)        
+    elif task == 'query':
+        f = Flow.load_config('flow-query.yml')
+        with f:
+            f.block()
+    else:
+        raise NotImplementedError(f'Unknown task: `{task}`. A valid task is either `index`, `query`.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ object-search/make_html.py
@@ -0,0 +1,55 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import os
+import webbrowser
+
+from jina.flow import Flow
+from pkg_resources import resource_filename
+
+# replace this to whatever dir has png, it doesn't care whether its is pokemon, as long as there is a PNG it's fine
+image_src = 'data/**/*.png'
+parallel = 1
+shards = 8
+
+os.environ['PARALLEL'] = str(parallel)
+os.environ['SHARDS'] = str(shards)
+os.environ['WORKDIR'] = './workspace'
+os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
+
+f = Flow.load_config('flow-query.yml')
+f.use_grpc_gateway()  # use gRPC gateway for better batch efficiency
+
+result_html = []
+
+
+def print_result(resp):
+    for d in resp.search.docs:
+        vi = d.data_uri
+        result_html.append(f'<tr><td><img src="{vi}"/></td><td>')
+        for match in d.matches:
+            uri = match.data_uri
+            result_html.append(f'<img src="{uri}" style="opacity:{match.score.value}"/>')
+            # k['score']['explained'] = json.loads(kk.score.explained)
+        result_html.append('</td></tr>\n')
+
+
+def write_html(html_path):
+    with open(resource_filename('jina', '/'.join(('resources', 'helloworld.html'))), 'r') as fp, \
+            open(html_path, 'w') as fw:
+        t = fp.read()
+        t = t.replace('{% RESULT %}', '\n'.join(result_html))
+        fw.write(t)
+
+    url_html_path = 'file://' + os.path.abspath(html_path)
+
+    try:
+        webbrowser.open(url_html_path, new=2)
+    except:
+        pass
+
+
+with f:
+    f.search_files(image_src, sampling_rate=.01, batch_size=8, output_fn=print_result, top_k=20)
+
+write_html('result.html')
