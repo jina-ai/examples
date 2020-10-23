4242a0bdf634ccbf24a7f5f444de13043b470f5d
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-10-23 13:07:25 +0530

    test: added object search test
    Added test for object search which indexes 3 images and then queries with its object images to check if the query is the same as the first result. This uses `flow-query-object.yml`

--- southpark-search/tests/test_southparksearch.py
+++ southpark-search/tests/test_southparksearch.py
@@ -1,37 +1,29 @@
-__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
-__license__ = "Apache-2.0"
-
-import pytest
-
 import json
 import os
 import sys
 import subprocess
 
 from jina.flow import Flow
+import pytest
+
 
 NUM_DOCS = 100
 TOP_K = 3
-INDEX_FLOW_FILE_PATH = "flow-index.yml"
-QUERY_FLOW_FILE_PATH = "flow-query.yml"
+INDEX_FLOW_FILE_PATH = 'flow-index.yml'
+QUERY_FLOW_FILE_PATH = 'flow-query.yml'
 
 
 def config(tmpdir):
-    os.environ["JINA_DATA_FILE"] = os.environ.get("JINA_DATA_FILE", "tests/data-index.csv")
-    os.environ["JINA_WORKSPACE"] = os.environ.get("JINA_WORKSPACE", str(tmpdir))
-    os.environ["JINA_PORT"] = os.environ.get("JINA_PORT", str(45678))
-
-
-def setup_env():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests", "jina[http]"])
+    os.environ['JINA_DATA_FILE'] = 'tests/data-index.csv'
+    os.environ['JINA_WORKSPACE'] = str(tmpdir)
+    os.environ['JINA_PORT'] = str(45678)
 
 
 def index_documents():
     f = Flow().load_config(INDEX_FLOW_FILE_PATH)
 
     with f:
-        f.index_lines(filepath=os.environ["JINA_DATA_FILE"],
+        f.index_lines(filepath=os.environ['JINA_DATA_FILE'],
                       batch_size=8,
                       size=NUM_DOCS)
 
@@ -44,11 +36,11 @@ def call_api(url, payload=None, headers={'Content-Type': 'application/json'}):
 def get_results(query, top_k=TOP_K):
     return call_api(
         'http://0.0.0.0:45678/api/search',
-        payload={"top_k": TOP_K, "mode": "search", "data": [f"text:{query}"]}
+        payload={"top_k": top_k, "mode": "search", "data": [f"text:{query}"]}
     )
 
 
-def set_flow():
+def get_flow():
     f = Flow().load_config(QUERY_FLOW_FILE_PATH)
     f.use_rest_gateway()
     return f
@@ -56,17 +48,15 @@ def set_flow():
 
 @pytest.fixture
 def queries():
-    return [('hey dude',
-             ["Don't say anything\n", "Check that: I'll watch that game.\n", "Because you're a fucking fatass\n"]),
-            ('sister', ['Sorry\n', 'Lame.\n', 'Christ\n']),
-            ("Ill watch that game", ["Check that: I'll watch that game.\n", 'Quit it\n', 'Sorry\n'])]
+    return [("Don't say anything\n", ["Don't say anything\n", "Check that: I'll watch that game.\n", "I'm trying to talk\n"]),
+            ('Sorry\n', ['Sorry\n', 'Hey\n', 'Allб\n']),
+            ("Check that: I'll watch that game.\n", ["Check that: I'll watch that game.\n", "I just think it's a fabulous app\n", 'Could you get that\n'])]
 
 
-def test_query(queries, tmpdir):
+def test_query(tmpdir, queries):
     config(tmpdir)
-    setup_env()
     index_documents()
-    f = set_flow()
+    f = get_flow()
     with f:
         for query, exp_result in queries:
             output = get_results(query)
