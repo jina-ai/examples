1a39fe7f6366739e05e24ad9f87554d84178e07a
Maximilian Werk <maximilian.werk@jina.ai>
2020-11-09 10:40:33 +0100

    test: runs with jina and jinad main

--- southpark-search/tests/distributed/test_integration.sh
+++ southpark-search/tests/distributed/test_integration.sh
@@ -25,11 +25,11 @@ FLOW_ID=$(curl -s --request PUT "http://localhost:8000/v1/flow/yaml" \
 
 echo "Successfully started the flow: ${FLOW_ID}"
 
-curl --request POST -d '{"top_k": 10, "mode": "search",  "data": ["text:hey, dude"]}' -H 'Content-Type: application/json' '0.0.0.0:45678/api/search' | \
-    jq ".search.docs[] | .matches[] | .text"
+curl --request POST -d '{"top_k": 10, "data": ["text:hey, dude"]}' -H 'Content-Type: application/json' '0.0.0.0:45678/api/search' | \
+    jq -e ".search.docs[] | .matches[] | .text"
 
-curl --request GET "http://0.0.0.0:8000/v1/flow/${FLOW_ID}" -H "accept: application/json" | jq .
+curl --request GET "http://0.0.0.0:8000/v1/flow/${FLOW_ID}" -H "accept: application/json" | jq -e ".status_code"
 
-curl --request DELETE "http://0.0.0.0:8000/v1/flow?flow_id=${FLOW_ID}" -H "accept: application/json" | jq .
+curl --request DELETE "http://0.0.0.0:8000/v1/flow?flow_id=${FLOW_ID}" -H "accept: application/json" | jq -e ".status_code"
 
 docker-compose -f tests/distributed/docker-compose.yml --project-directory . down
