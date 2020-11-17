7259f8eb39df57caf19826e31bbc53f155cd33ba
Susana Guzman <zuxi.w13@gmail.com>
2020-11-17 16:11:22 +0100

    fix: spun 3 containers

--- southpark-search/tests/distributed/terraform.tf
+++ southpark-search/tests/distributed/terraform.tf
@@ -3,107 +3,70 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-provider "docker" {
-  host = var.docker_host
-  registry_auth {
-    address  = "https://registry.docker.io/v2/jina-ai/jina"
-    username = var.docker_username
-    password = var.docker_password
-  }
-}
-
 #Creates AWS ECR repo for SouthPark image
-resource "aws_ecr_repository" "southpark" {
-  name = "sp-repo"
-  tags = {
-    Name = "southpark_repo"
-  }
-}
-# Create a docker container with Jina SouthPark image
-resource "docker_container" "jina_southpark" {
-  image = "${docker_image.southparkimage}"
-  name  = "jina_southpark"
-}
+#resource "aws_ecr_repository" "southpark" {
+#  name = "sp-repo-db"
+#  tags = {
+#    Name = "southpark_repo"
+#  }
+#}
 
-# Find the latest southpark precise image.
-resource "docker_image" "southparkimage" {
-  name = "jinaai/hub.app.distilbert-southpark"
-}
-
-# Build and push the Docker image to aws ecr repository
-resource "null_resource" "push" {
-  provisioner "local-exec" {
-    command     = "${coalesce(var.push_script, "${path.module}/push.sh")} ${var.source_path} ${aws_ecr_repository.southpark.repository_url} ${var.tag}"
-    interpreter = ["bash", "-c"]
-  }
-}
-
-resource "aws_default_vpc" "default_vpc" {
-}
-
-# AWS Instance for Encoder
-resource "aws_instance" "encoder" {
-  ami           = "ami-07efac79022b86107"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "encoder"
-  }
-}
-
-# AWS Instance for Indexer
-resource "aws_instance" "indexer" {
-  ami           = "ami-07efac79022b86107"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "indexer"
-  }
-}
-
-# AWS Instance for running Flow
-resource "aws_instance" "flow" {
-  ami           = "ami-07efac79022b86107"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "flow"
-  }
-  provisioner "remote-exec" {
-    inline = [<<EOF
-    curl -s --request PUT "http://localhost:8000/v1/flow/yaml" \
-    -H  "accept: application/json" \
-    -H  "Content-Type: multipart/form-data" \
-    -F "uses_files=@pods/encode.yml" \
-    -F "uses_files=@pods/extract.yml" \
-    -F "uses_files=@pods/index.yml" \
-    -F "pymodules_files=@pods/text_loader.py" \
-    -F "yamlspec=@tests/distributed/flow-query.yml"
-    EOF
-    ]
-  }
-}
-
-# Sets environment variables for encoder and indexer
-resource "null_resource" "environment" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command = ""
-    environment = {
-      JINA_ENCODER_HOST = "${aws_instance.encoder.private_ip}"
-      JINA_INDEX_HOST = "${aws_instance.indexer.private_ip}"
-    }
-  }
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = "${aws_default_vpc.default_vpc.id}"
-}
-
+#Create Cluster
 resource "aws_ecs_cluster" "southpark_cluster" {
   name = "southpark_cluster"
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#Create task
+resource "aws_ecs_task_definition" "southpark_task" {
+  family                   = "southpark_task" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "southpark_task",
+      "image": "416454113568.dkr.ecr.us-east-2.amazonaws.com/sp-repo",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 45678,
+          "hostPort": 45678
+        }
+      ],
+      "memory": 512,
+      "cpu": 10
+    },
+    {
+      "name": "encoder",
+      "image": "416454113568.dkr.ecr.us-east-2.amazonaws.com/sp-repo",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 10
+    },
+    {
+      "name": "indexer",
+      "image": "416454113568.dkr.ecr.us-east-2.amazonaws.com/sp-repo",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 10
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsExecutionRole.arn}"
 }
 
 resource "aws_iam_role" "ecsExecutionRole" {
@@ -123,33 +86,59 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-#Create ECS task
-resource "aws_ecs_task_definition" "southpark_task" {
-  family                   = "southpark_task" 
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "southpark_task",
-      "image": "${aws_ecr_repository.southpark.repository_url}",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 45678,
-          "hostPort": 45678
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.ecsExecutionRole.arn}"
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+#create service
+resource "aws_ecs_service" "southpark_service" {
+  name            = "southpark_service"
+  cluster         = "${aws_ecs_cluster.southpark_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.southpark_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 3 #3 Containers
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.southpark_task.family}"
+    container_port   = 45678 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = data.aws_subnet_ids.default.ids
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+  depends_on = [aws_lb_listener.lsr, aws_iam_role_policy_attachment.ecsTaskExecutionRole_policy]
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+
+#ECS will receive traffic from the ALB
+resource "aws_security_group" "service_security_group" {
+  description = "Allow acces only from the ALB"
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress { 
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${aws_default_vpc.default_vpc.id}"
+}
 
 #create load balancer
 resource "aws_alb" "application_load_balancer" {
@@ -160,13 +149,22 @@ resource "aws_alb" "application_load_balancer" {
   security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
-resource "aws_lb_listener" "lsr" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
-  port              = "45678"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+# Creating a security group for the load balancer:
+# This is the one that will receive traffic from internet
+resource "aws_security_group" "load_balancer_security_group" {
+  description = "control access to the ALB"
+  ingress {
+    from_port   = 45678
+    to_port     = 45678
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -188,66 +186,16 @@ resource "aws_lb_target_group" "target_group" {
   }
 }
 
-
-# Creating a security group for the load balancer:
-# This is the one that will receive traffic from internet
-resource "aws_security_group" "load_balancer_security_group" {
-  description = "control access to the ALB"
-  ingress {
-    from_port   = 45678
-    to_port     = 45678
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_lb_listener" "lsr" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "45678"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
   }
 }
 
-#ECS will receive traffic from the ALB
-resource "aws_security_group" "service_security_group" {
-  description = "Allow acces only from the ALB"
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-  }
-
-  egress { 
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-#create service
-resource "aws_ecs_service" "southpark_service" {
-  name            = "southpark_service"
-  cluster         = "${aws_ecs_cluster.southpark_cluster.id}"
-  task_definition = "${aws_ecs_task_definition.southpark_task.arn}"
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.southpark_task.family}"
-    container_port   = 45678 # Specifying the container port
-  }
-
-  network_configuration {
-    subnets          = data.aws_subnet_ids.default.ids
-    assign_public_ip = true
-    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
-  }
-  depends_on = [aws_lb_listener.lsr, aws_iam_role_policy_attachment.ecsTaskExecutionRole_policy]
-}
 
 output "alb_url" {
   value = "http://${aws_alb.application_load_balancer.dns_name}"
