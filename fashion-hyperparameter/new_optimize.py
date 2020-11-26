01c73080dfeef21925342e137a24210f8028c5f7
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-11-26 22:21:04 +0530

    feat: with flow runner

--- fashion-hyperparameter/new_optimize.py
+++ fashion-hyperparameter/new_optimize.py
@@ -4,16 +4,15 @@ from fashion.config import get_run_parameters
 from fashion.data import get_data
 from fashion.evaluation import index_document_generator, evaluation_document_generator
 
-# from jina.optimize import Optimizer
-from optimize_class import Optimizer
-
+from optimize_class import FlowRunner, Optimizer
 
 
 def config_environment():
     os.environ.setdefault('JINA_DATA_DIRECTORY', 'data')
+    os.environ.setdefault('JINA_LOG_CONFIG', 'logging.optimizer.yml')
 
 
-def sample_run_parameters(trial):
+def trial_parameter_sampler(trial):
     target_dimension = trial.suggest_int('target_dimension', 32, 256, step=32)
     return get_run_parameters(target_dimension)
 
@@ -21,11 +20,11 @@ def sample_run_parameters(trial):
 def main():
     config_environment()
     data = get_data()
-    opt = Optimizer('flows/index.yml', 'flows/evaluate.yml',
+    flow_runner = FlowRunner('flows/index.yml', 'flows/evaluate.yml',
                     index_document_generator(1000, data), evaluation_document_generator(1000, data),
                     500, 500,
-                    sample_run_parameters, 
-                    5)
+                    overwrite_workspace=True)
+    opt = Optimizer(flow_runner, trial_parameter_sampler, 3)
     opt.optimize_flow()
 
 
