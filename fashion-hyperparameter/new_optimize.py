4b6bdc7f5b94e80ff3b3ee57975e2dd89f072bfb
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-12-04 15:18:07 +0530

    feat: env yml reading and best config yml dumping

--- fashion-hyperparameter/new_optimize.py
+++ fashion-hyperparameter/new_optimize.py
@@ -7,24 +7,16 @@ from optimize_class import FlowRunner, Optimizer
 
 DATA_DIRECTORY = 'data'
 
-def config_environment():
-    #Todo: remove all env
-    os.environ.setdefault('JINA_PARALLEL', '1')
-    os.environ.setdefault('JINA_SHARDS', '1')
-    os.environ.setdefault('JINA_LOG_CONFIG', 'logging.optimizer.yml')
-
-
 def main():
-    config_environment()
     data = get_data(DATA_DIRECTORY)
     flow_runner = FlowRunner(
                     index_document_generator(1000, data), evaluation_document_generator(1000, data),
                     500, 500,
-                    overwrite_workspace=True)
+                    env_yaml='flows/env.yml',
+                    overwrite_workspace=False)
     opt = Optimizer(flow_runner, 
-                    'pods', 'flows/index.yml', 'flows/evaluate.yml', 'flows/parameter.yml',
-                     2)
-    opt.optimize_flow()
+                    'pods', 'flows/index.yml', 'flows/evaluate.yml', 'flows/parameter.yml')
+    opt.optimize_flow(n_trials=2)
 
 
 if __name__ == '__main__':
