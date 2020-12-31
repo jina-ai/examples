af7a7d333ec8ba92a8e4fb7880ee0aa4dacaf511
carlosb1 <mcflurry0@gmail.com>
2020-12-31 20:23:49 +0100

    Added commands for new functions

--- news_scrap_searcher/app.py
+++ news_scrap_searcher/app.py
@@ -7,6 +7,7 @@ from jina.flow import Flow
 
 num_docs = int(os.environ.get('MAX_DOCS', 500))
 
+
 def config():
     parallel = 2 if sys.argv[1] == 'index' else 1
     shards = 2
@@ -23,18 +24,62 @@ def index():
     f = Flow.load_config('flows/index.yml')
 
     with f:
-        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_PATH', None))
+        data_path = os.path.join(os.path.dirname(
+            __file__), os.environ.get('JINA_DATA_PATH', None))
         if data_path:
-            f.index_lines(filepath=data_path, batch_size=16, read_mode='r', size=num_docs)
+            f.index_lines(filepath=data_path, batch_size=16,
+                          read_mode='r', size=num_docs)
         else:
-            f.index_lines(lines=['abc', 'cde', 'efg'], batch_size=16, read_mode='r', size=num_docs)
+            f.index_lines(lines=['abc', 'cde', 'efg'],
+                          batch_size=16, read_mode='r', size=num_docs)
 
 # for search
+
+
 def search():
     f = Flow.load_config('flows/query.yml')
 
     with f:
         f.block()
+
+
+def query_restful():
+    f = Flow().load_config('flows/query.yml')
+    f.use_rest_gateway()
+    with f:
+        f.block()
+
+
+def query(top_k):
+    f = Flow().load_config('flows/query.yml')
+    with f:
+        while True:
+            text = input('please type a sentence: ')
+            if not text:
+                break
+
+            def ppr(x):
+                print_topk(x, text)
+
+            f.search_lines(
+                lines=[
+                    text,
+                ],
+                output_fn=ppr,
+                top_k=top_k,
+            )
+
+
+def print_topk(resp, sentence):
+    for d in resp.search.docs:
+        print(f'Ta-Dah🔮, here are what we found for: {sentence}')
+        for idx, match in enumerate(d.matches):
+
+            score = match.score.value
+            if score < 0.0:
+                continue
+            # dialog = match.text.strip()
+            print(f'> {idx:>2d}({score:.2f}). {match}"')
 
 
 # for test before put into docker
