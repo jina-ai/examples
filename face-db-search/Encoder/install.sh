c16ba91759e2099bb4272fe85b02b07d4ad9b7d4
Alex C-G <alexcg@outlook.com>
2021-01-07 14:30:49 +0000

    chore: rm redundant examples

--- face-db-search/Encoder/install.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/shivam-raj/facenet-pytorch.git
-cd facenet-pytorch/
-pip install .
--- flower-search/get_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-TEST_WORKDIR=/tmp/jina/flower/
-mkdir -p ${TEST_WORKDIR}
-cd ${TEST_WORKDIR}
-curl https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz --output 17flowers.tgz
-tar zxf 17flowers.tgz
