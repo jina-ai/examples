d98aca020a126d518ef71f2e04f4bc8f82269cd8
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-01-18 18:54:05 +0100

    refactor: flow version 1

---
+++ cross-modal-search/get_data.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+DATASET="adityajn105/flickr8k"
+DATA_DIR="data"
+
+if [ -d ${DATA_DIR} ]; then
+  echo ${DATA_DIR}' exists, please remove it before running the script'
+  exit 1
+fi
+
+mkdir -p ${DATA_DIR}
+kaggle datasets download -d ${DATASET}
+unzip flickr8k.zip
+rm flickr8k.zip
+mv Images data/f8k/images
+mv captions.txt data/f8k/captions.txt
