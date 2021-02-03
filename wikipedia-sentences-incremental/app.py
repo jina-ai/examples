71b0bf0ec7e40a0303e74a2ee4ebd27d86b86dc5
Alex C-G <alexcg@outlook.com>
2021-02-03 09:23:34 +0000

    fix(wikipedia-incremental): single quotes, sys.exit if workdir exist

--- wikipedia-sentences-incremental/app.py
+++ wikipedia-sentences-incremental/app.py
@@ -1,6 +1,7 @@
-__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
-__license__ = "Apache-2.0"
+__copyright__ = 'Copyright (c) 2020 Jina AI Limited. All rights reserved.'
+__license__ = 'Apache-2.0'
 
+import sys
 import os
 import itertools as it
 
@@ -19,12 +20,12 @@ def config():
     os.environ['JINA_WORKSPACE'] = os.environ.get('JINA_WORKSPACE', 'workspace')
 
     os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
-    # os.environ["JINA_MAX_DOCS"] = os.environ.get("JINA_MAX_DOCS", str(100))
+    # os.environ['JINA_MAX_DOCS'] = os.environ.get('JINA_MAX_DOCS', str(100))
 
 
 def print_topk(resp, sentence):
     for d in resp.search.docs:
-        print(f"Ta-Dah🔮, here are what we found for: {sentence}")
+        print(f'Ta-Dah🔮, here are what we found for: {sentence}')
         for idx, match in enumerate(d.matches):
 
             score = match.score.value
@@ -37,13 +38,17 @@ def index(num_docs):
     f = Flow().load_config('flows/index.yml')
 
     with f:
-        print(f"Indexing {os.environ['JINA_DATA_FILE_1']}")
-        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE_1', None))
+        print(f'Indexing {os.environ['JINA_DATA_FILE_1']}')
+        data_path = os.path.join(
+            os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE_1', None)
+        )
         f.index_lines(filepath=data_path, batch_size=16, read_mode='r', size=num_docs)
 
     with f:
-        print(f"Indexing {os.environ['JINA_DATA_FILE_2']}")
-        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE_2', None))
+        print(f'Indexing {os.environ['JINA_DATA_FILE_2']}')
+        data_path = os.path.join(
+            os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE_2', None)
+        )
         f.index_lines(filepath=data_path, batch_size=16, read_mode='r', size=num_docs)
 
 
@@ -58,7 +63,13 @@ def query(top_k):
             def ppr(x):
                 print_topk(x, text)
 
-            f.search_lines(lines=[text, ], on_done=ppr, top_k=top_k)
+            f.search_lines(
+                lines=[
+                    text,
+                ],
+                on_done=ppr,
+                top_k=top_k,
+            )
 
 
 def query_restful():
@@ -75,38 +86,47 @@ def dryrun():
 
 
 max_docs = int(os.environ.get('JINA_MAX_DOCS', 50))
+
+
 @click.command()
 @click.option(
-    "--task",
-    "-t",
+    '--task',
+    '-t',
     type=click.Choice(
-        ["index", "query", "query_restful", "dryrun"], case_sensitive=False
+        ['index', 'query', 'query_restful', 'dryrun'], case_sensitive=False
     ),
 )
-@click.option("--num_docs", "-n", default=max_docs)
-@click.option("--top_k", "-k", default=5)
+@click.option('--num_docs', '-n', default=max_docs)
+@click.option('--top_k', '-k', default=5)
 def main(task, num_docs, top_k):
     config()
-    workspace = os.environ["JINA_WORKSPACE"]
-    if task == "index":
+    workspace = os.environ['JINA_WORKSPACE']
+    if task == 'index':
         if os.path.exists(workspace):
-            print(f'\n +----------------------------------------------------------------------------------+ \
+            print(
+                f'\n +----------------------------------------------------------------------------------+ \
                     \n |                                   🤖🤖🤖                                         | \
                     \n | The directory {workspace} already exists. Please remove it before indexing again.  | \
                     \n |                                   🤖🤖🤖                                         | \
-                    \n +----------------------------------------------------------------------------------+')
+                    \n +----------------------------------------------------------------------------------+'
+            )
+            sys.exit()
         index(num_docs)
-    if task == "query":
+    if task == 'query':
         if not os.path.exists(workspace):
-            print(f"The directory {workspace} does not exist. Please index first via `python app.py -t index`")
+            print(
+                f'The directory {workspace} does not exist. Please index first via `python app.py -t index`'
+            )
         query(top_k)
-    if task == "query_restful":
+    if task == 'query_restful':
         if not os.path.exists(workspace):
-            print(f"The directory {workspace} does not exist. Please index first via `python app.py -t index`")
+            print(
+                f'The directory {workspace} does not exist. Please index first via `python app.py -t index`'
+            )
         query_restful()
-    if task == "dryrun":
+    if task == 'dryrun':
         dryrun()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
