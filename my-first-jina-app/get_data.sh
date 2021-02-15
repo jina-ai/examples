9b2b45e42148100cfbf82fb97b45818f622cb4ea
Alex Cureton-Griffiths <alexcg1@users.noreply.github.com>
2021-02-15 03:54:58 +0000

    fix(old-examples): move all to legacy repo (#385)
    * fix(my-first-app): mv to legacy
    
    * fix(all-to-move): move all to legacy

--- my-first-jina-app/get_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-WORKDIR=./data
-mkdir -p ${WORKDIR}
-wget -P $WORKDIR/raw https://github.com/alexcg1/ml-datasets/raw/master/nlp/startrek/startrek_tng.csv
-cat $WORKDIR/raw/startrek_tng.csv | cut -d ! -f2- > $WORKDIR/startrek_tng.csv
