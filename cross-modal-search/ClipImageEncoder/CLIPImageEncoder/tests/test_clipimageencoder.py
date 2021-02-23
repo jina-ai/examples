a2627e9fd83b2793717bfa359847ab38e5876777
David Buchaca Prats <davidbuchaca@gmail.com>
2021-02-23 12:02:33 +0100

    test updated with printable array information

--- cross-modal-search/ClipImageEncoder/CLIPImageEncoder/tests/test_clipimageencoder.py
+++ cross-modal-search/ClipImageEncoder/CLIPImageEncoder/tests/test_clipimageencoder.py
@@ -9,6 +9,13 @@ from .. import CLIPImageEncoder
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
+def print_array_info(x, x_varname):
+    print('\n')
+    print(f'type({x_varname})={type(x)}')
+    print(f'{x_varname}.dtype={x.dtype}')
+    print(f'len({x_varname})={len(x)}')
+    print(f'{x_varname}.shape={x.shape}')
+
 
 def test_clip_available_models():
 
@@ -19,32 +26,44 @@ def test_clip_available_models():
 
 def test_clip_image_encoder():
 
+    # Input
     man_piercing_image_path = os.path.join(cur_dir, '../imgs/man_piercing.jpg')
     im = PIL.Image.open(man_piercing_image_path)
-    
     device='cpu'
     _, preprocess = clip.load('ViT-B/32', device=device)
     im_tensor_clip_input = preprocess(im).unsqueeze(0).to(device)
     im_tensor_clip_np = im_tensor_clip_input.detach().numpy()
 
+    # Encoder embedding 
     encoder = CLIPImageEncoder()
+    print_array_info(im_tensor_clip_np, 'im_tensor_clip_np')
     embeddeding_np = encoder.encode(im_tensor_clip_np)
+    print_array_info(embeddeding_np, 'embeddeding_np')
+
+    # Compare with ouptut 
     expected = np.load(os.path.join(cur_dir, 'expected.npy'))
     np.testing.assert_almost_equal(embeddeding_np, expected, decimal=3)
 
 
 def test_clip_image_encoder_batch():
 
+    # Input
     man_piercing_image_path = os.path.join(cur_dir, '../imgs/man_piercing.jpg')
     im = PIL.Image.open(man_piercing_image_path)
-
     device='cpu'
     _, preprocess = clip.load('ViT-B/32', device=device)
     im_tensor_clip_input = preprocess(im).unsqueeze(0).to(device)
     im_tensor_clip_np = im_tensor_clip_input.detach().numpy()
     batch = np.vstack([im_tensor_clip_input, im_tensor_clip_input, im_tensor_clip_input])
 
+    # Encoder embedding 
     encoder = CLIPImageEncoder()
+    print_array_info(batch, 'batch')
     embeddeding_batch_np = encoder.encode(batch)
+    print_array_info(embeddeding_batch_np, 'embeddeding_batch_np')
+
+    # Compare with ouptut 
     expected = np.load(os.path.join(cur_dir, 'expected_batch.npy'))
     np.testing.assert_almost_equal(embeddeding_batch_np, expected, decimal=3)
+
+
--- cross-modal-search/ClipImageEncoder/CLIPTextEncoder/tests/test_clipitextencoder.py
+++ cross-modal-search/ClipImageEncoder/CLIPTextEncoder/tests/test_clipitextencoder.py
@@ -9,6 +9,14 @@ from .. import CLIPTextEncoder
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
+
+def print_array_info(x, x_varname):
+    print('\n')
+    print(f'type({x_varname})={type(x)}')
+    print(f'{x_varname}.dtype={x.dtype}')
+    print(f'len({x_varname})={len(x)}')
+    print(f'{x_varname}.shape={x.shape}')
+
 def test_clip_available_models():
 
     models = ['ViT-B/32', 'RN50']
@@ -17,16 +25,30 @@ def test_clip_available_models():
 
 def test_clip_text_encoder():
 
-    text = 'Han likes eating pizza'
+    # Input
+    text = np.array(['Han likes eating pizza'])
+
+    # Encoder embedding 
     encoder = CLIPTextEncoder()
+    print_array_info(text, 'text')
     embeddeding_np = encoder.encode(text)
+    print_array_info(embeddeding_np, 'embeddeding_np')
+
+    # Compare with ouptut 
     expected = np.load(os.path.join(cur_dir, 'expected.npy'))
     np.testing.assert_almost_equal(embeddeding_np, expected)
 
 def test_clip_text_encoder_batch():
 
+    # Input
     text_batch = np.array(['Han likes eating pizza', 'Han likes pizza', 'Jina rocks'])
+
+    # Encoder embedding 
     encoder = CLIPTextEncoder()
+    print_array_info(text_batch, 'text_batch')
     embeddeding_batch_np = encoder.encode(text_batch)
+    print_array_info(embeddeding_batch_np, 'embeddeding_batch_np')
+
+    # Compare with ouptut 
     expected_batch = np.load(os.path.join(cur_dir, 'expected_batch.npy'))
     np.testing.assert_almost_equal(embeddeding_batch_np, expected_batch)
