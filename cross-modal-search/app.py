eb6dc538a936313c21597dbf71fa5c7938a6f95f
bwanglzu <bo.wang@jina.ai>
2021-03-02 09:52:10 +0100

    feat: allow user to specify model

--- cross-modal-search/app.py
+++ cross-modal-search/app.py
@@ -10,10 +10,21 @@ from jina import Document
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
-def config():
+def config(model_name):
     os.environ['JINA_PARALLEL'] = os.environ.get('JINA_PARALLEL', '1')
     os.environ['JINA_SHARDS'] = os.environ.get('JINA_SHARDS', '1')
     os.environ['JINA_PORT'] = '45678'
+    if model_name == 'clip':
+        pass
+    elif model_name == 'vse':
+        os.environ['JINA_IMAGE_ENCODER'] = 'docker://jinahub/pod.encoder.vseimageencoder:0.0.5-1.0.6'
+        os.environ['JINA_TEXT_ENCODER'] = 'docker://jinahub/pod.encoder.vsetextencoder:0.0.6-1.0.6'
+        os.environ['JINA_TEXT_ENCODER_INTERNAL'] = 'yaml/vse/text-encoder.yml'
+    else:
+        msg = f'Unsupported model {model_name}.'
+        msg += 'Expected `clip` or `vse`.'
+        raise ValueError(msg)
+
 
 
 def input_index_data(num_docs=None, batch_size=8, dataset_type='f30k'):
@@ -51,8 +62,9 @@ def input_index_data(num_docs=None, batch_size=8, dataset_type='f30k'):
 @click.option('--num_docs', '-n', default=50)
 @click.option('--request_size', '-s', default=16)
 @click.option('--data_set', '-d', type=click.Choice(['f30k', 'f8k'], case_sensitive=False), default='f8k')
-def main(task, num_docs, request_size, data_set):
-    config()
+@click.option('--model_name', '-m', type=click.Choice(['clip', 'vse'], case_sensitive=False), default='clip')
+def main(task, num_docs, request_size, data_set, model_name):
+    config(model_name)
     if task == 'index':
         with Flow().load_config('flow-index.yml') as f:
             f.index(
