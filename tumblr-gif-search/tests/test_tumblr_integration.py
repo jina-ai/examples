e00ff0714676d78d430d3bef97a33463fb89b705
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2021-03-04 20:16:03 +0800

    fix: fix tumblr gif search example

--- tumblr-gif-search/tests/test_tumblr_integration.py
+++ tumblr-gif-search/tests/test_tumblr_integration.py
@@ -25,40 +25,32 @@ def env_setup(tmpdir):
     os.environ['SHARDS_CHUNK_SEG'] = str(SHARDS_CHUNK_SEG)
     os.environ['SHARDS_INDEXER'] = str(SHARDS_INDEXER)
     os.environ['JINA_WORKSPACE'] = './workspace'
-    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45680))
 
 
 def index_documents():
     f = Flow().load_config('flow-index.yml')
 
     with f:
-        f.index_files(GIF_BLOB, request_size=REQ_SIZE, read_mode='rb', skip_dry_run=True)
-
-
-def call_api(url, payload=None):
-    headers = {'Content-Type': 'application/json'}
-    return requests.post(url, data=json.dumps(payload), headers=headers).json()
-
-
-def get_results(query, top_k=JINA_TOPK):
-    return call_api(
-        f'http://0.0.0.0:{os.environ["JINA_PORT"]}/api/search',
-        payload={'top_k': top_k, 'mode': 'search', 'data': [query]})
+        f.index_files(GIF_BLOB, request_size=REQ_SIZE, read_mode='rb')
 
 
 def get_flow():
     f = Flow().load_config('flow-query.yml')
-    f.use_rest_gateway()
+    f.use_grpc_gateway()
     return f
+
+
+def validate(req):
+    for doc in req.search.docs:
+        print(doc)
+        assert len(doc.matches) == JINA_TOPK
+        for match in doc.matches:
+            assert match.uri.startswith('data:image/gif')
 
 
 def test_query(env_setup):
     index_documents()
     f = get_flow()
     with f:
-        for object_image_path in glob(GIF_BLOB)[:NR_CASES_SEARCH]:
-            output = get_results(object_image_path)
-            matches = output['search']['docs'][0]['matches']
-            assert len(matches) == JINA_TOPK
-            for match in matches:
-                assert match['uri'].startswith('data:image/gif')
+        f.search_files(glob(GIF_BLOB)[:NR_CASES_SEARCH], on_done=validate)
