6cbf5ab06a59d52bfafac2ddb884c23e74538278
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2021-03-04 20:58:32 +0800

    fix: update the audio example

--- audio-search/tests/test_audio_search.py
+++ audio-search/tests/test_audio_search.py
@@ -17,7 +17,7 @@ NUM_DOCS = 3
 def env_setup(tmpdir):
     os.environ['JINA_SHARDS'] = str(2)
     os.environ['JINA_WORKSPACE'] = str(tmpdir)
-    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(65481))
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(65480))
     os.environ['JINA_TOPK'] = str(JINA_TOPK)
     if platform == "linux":
         os.system('sudo apt-get install libsndfile1')
@@ -34,35 +34,21 @@ def index_documents():
         f.index_files('tests/data/*.wav', batch_size=2, size=NUM_DOCS)
 
 
-def call_api(url, payload=None):
-    headers = {'Content-Type': 'application/json'}
-    return requests.post(url, data=json.dumps(payload), headers=headers).json()
-
-
-def get_results(query, top_k=JINA_TOPK):
-    return call_api(
-        f'http://0.0.0.0:{os.environ["JINA_PORT"]}/api/search',
-        payload={'top_k': top_k, 'mode': 'search', 'data': [query]})
-
-
 def get_flow():
     f = Flow().load_config('flows/query.yml')
-    f.use_rest_gateway()
+    f.use_grpc_gateway()
     return f
 
 
-@pytest.fixture()
-def queries():
-    return ['Y-0BIyqJj9ZU', 'Y-0CamVQdP_Y', 'Y-0Gj8-vB1q4']
-
-
-def test_query(env_setup, download_model, queries):
+def test_query(env_setup, download_model):
     index_documents()
     f = get_flow()
-    paths = queries
+    fn_list = ['Y-0BIyqJj9ZU', 'Y-0CamVQdP_Y', 'Y-0Gj8-vB1q4']
+    test_filepath_list = [f'tests/data/{_fn}.wav' for _fn in fn_list]
+
+    def validate(req):
+        for doc in req.search.docs:
+            assert doc.matches[0].uri.startswith('data:audio/x-wav')
+
     with f:
-        for path in paths:
-            object_path = 'tests/data/' + path + '.wav'
-            output = get_results(object_path)
-            matches = output['search']['docs'][0]['matches']
-            assert len(matches) >= 1
+        f.search_files(test_filepath_list, on_done=validate)
