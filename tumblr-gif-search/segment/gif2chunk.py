8748b6fb252d311cd78517390fd5fd011b3e6816
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-03-18 09:50:25 -0400

    fix: change to single

--- tumblr-gif-search/segment/gif2chunk.py
+++ tumblr-gif-search/segment/gif2chunk.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageFile
 # workaround initialization code
 from PIL.GifImagePlugin import GifImageFile, _accept, _save, _save_all
 from jina.executors.segmenters import BaseSegmenter
-from jina.executors.decorators import single_multi_input
+from jina.executors.decorators import single
 
 
 class GifPreprocessor(BaseSegmenter):
@@ -20,7 +20,7 @@ class GifPreprocessor(BaseSegmenter):
         self.every_k_frame = every_k_frame
         self.max_frame = max_frame
 
-    @single_multi_input(num_data=2, flatten_output=False)
+    @single(slice_nargs=2, flatten_output=False)
     def segment(self, buffer, id):
         result = []
         try:
