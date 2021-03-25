52daa1a6dd31d30f29fa9ca492b1a3e6d96b7a32
Maximilian Werk <maximilian.werk@jina.ai>
2021-03-25 17:44:44 +0100

    fix: split indexer into c and cc

--- multimodal-search-pdf/app.py
+++ multimodal-search-pdf/app.py
@@ -35,13 +35,11 @@ def get_pdf(resp):
     print(len(resp.search.docs[0].chunks[0].chunks[0].matches))
     print(len(resp.search.docs[0].chunks[0].matches))
 
-
-    #import pdb
-    #pdb.set_trace()
+    # import pdb
+    # pdb.set_trace()
     print(resp.search.docs[0].matches[0].mime_type == 'application/pdf')
     print(len(resp.search.docs[0].matches))
     print(resp.search.docs[0].matches)
-
 
 
 @click.command()
@@ -58,29 +56,32 @@ def main(task, num_docs, top_k):
     config()
     if task == 'index':
         f = Flow.load_config('flows/index.yml')
+        f.plot()
         with f:
             for pdffile in ['data/blog1.pdf', 'data/blog2.pdf', 'data/blog3.pdf']:
-            #for pdffile in ['data/1806.05662.pdf', 'data/2103.01937.pdf', 'data/2103.07969.pdf']:
-            #for pdffile in ['data/1806.05662.pdf']:
-                f.index(input_fn=search_generator(path=pdffile, buffer=None), read_mode='r')
+                # for pdffile in ['data/1806.05662.pdf', 'data/2103.01937.pdf', 'data/2103.07969.pdf']:
+                # for pdffile in ['data/1806.05662.pdf']:
+                f.index(
+                    input_fn=search_generator(path=pdffile, buffer=None), read_mode='r'
+                )
 
     if task == 'query':
         f = Flow.load_config('flows/query-separate-segmenters.yml')
         f.plot()
         with f:
             d = Document()
-            #search_text = 'Developing a Jina app often means writing YAML configs.'
+            # search_text = 'Developing a Jina app often means writing YAML configs.'
             search_text = 'First things first, I hope you all saw we'
-            #search_text = 'Unsupervisedly Learned Relational Graphs'
-            #search_text = 'Recent advances in deep learning have largely relied on building blocks such as convolutional'
+            # search_text = 'Unsupervisedly Learned Relational Graphs'
+            # search_text = 'Recent advances in deep learning have largely relied on building blocks such as convolutional'
             d.text = search_text
             # There are three ways to search.
             print('text search:')
             f.search(input_fn=d, on_done=get_pdf)
             print('image search:')
-            #f.search(input_fn=search_generator(path='data/photo-2103.07969.png', buffer=None), read_mode='r',on_done=get_pdf)
+            # f.search(input_fn=search_generator(path='data/photo-2103.07969.png', buffer=None), read_mode='r',on_done=get_pdf)
             print('pdf search:')
-            #f.search(input_fn=search_generator(path='data/2103.01937.pdf', buffer=None), read_mode='r',on_done=get_pdf)
+            # f.search(input_fn=search_generator(path='data/2103.01937.pdf', buffer=None), read_mode='r',on_done=get_pdf)
 
 
 if __name__ == "__main__":
