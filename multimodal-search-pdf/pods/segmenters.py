c46a12fb3e76b12f8cc80a78c6d5863d3a7152c0
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-04-08 22:02:55 -0400

    feat[mmpdf]: fix errors

--- multimodal-search-pdf/pods/segmenters.py
+++ multimodal-search-pdf/pods/segmenters.py
@@ -36,16 +36,16 @@ class PDFSegmenter(BaseSegmenter):
         if mime_type != 'application/pdf':
             return chunks
         import fitz  # fitz is a library used in `PyMuPDF` to read pdf and images
-            import pdfplumber
+        import pdfplumber
 
-            if uri:
+        if uri:
             try:
                 pdf_img = fitz.open(uri)
                 pdf_text = pdfplumber.open(uri)
             except Exception as ex:
                 self.logger.error(f'Failed to open {uri}: {ex}')
                 return chunks
-            elif buffer:
+        elif buffer:
             try:
                 pdf_img = fitz.open(stream=buffer, filetype='pdf')
                 pdf_text = pdfplumber.open(io.BytesIO(buffer))
@@ -53,41 +53,41 @@ class PDFSegmenter(BaseSegmenter):
                 self.logger.error(f'Failed to load from buffer')
                 return chunks
             else:
-            self.logger.error('No value found in `buffer` or `uri`')
-            return chunks```
+                self.logger.error('No value found in `buffer` or `uri`')
+            return chunks
 
-            # Extract images
-            with pdf_img:
-                for page in range(len(pdf_img)):
-                    for img in pdf_img.getPageImageList(page):
-                        xref = img[0]
-                        pix = fitz.Pixmap(pdf_img, xref)
-                        # read data from buffer and reshape the array into 3-d format
-                        np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype('float32')
-                        if pix.n - pix.alpha < 4:  # if gray or RGB
-                            if pix.n == 1: #convert gray to rgb
-                                np_arr_rgb = np.concatenate((np_arr,)*3,-1)
-                                chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
-                            elif pix.n == 4: # remove transparency layer
-                                np_arr_rgb = np_arr[..., :3]
-                                chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
-                            else:
-                                chunks.append(dict(blob=np_arr, weight=1.0, mime_type='image/png'))
-                        else:  # if CMYK:
-                            pix = fitz.Pixmap(fitz.csRGB, pix)  # Convert to RGB
-                            np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype(
-                                'float32')
-                            chunks.append(
-                                dict(blob=np_arr, weight=1.0, mime_type='image/png'))
+        # Extract images
+        with pdf_img:
+            for page in range(len(pdf_img)):
+                for img in pdf_img.getPageImageList(page):
+                    xref = img[0]
+                    pix = fitz.Pixmap(pdf_img, xref)
+                    # read data from buffer and reshape the array into 3-d format
+                    np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype('float32')
+                    if pix.n - pix.alpha < 4:  # if gray or RGB
+                        if pix.n == 1: #convert gray to rgb
+                            np_arr_rgb = np.concatenate((np_arr,)*3,-1)
+                            chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
+                        elif pix.n == 4: # remove transparency layer
+                            np_arr_rgb = np_arr[..., :3]
+                            chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
+                        else:
+                            chunks.append(dict(blob=np_arr, weight=1.0, mime_type='image/png'))
+                    else:  # if CMYK:
+                        pix = fitz.Pixmap(fitz.csRGB, pix)  # Convert to RGB
+                        np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype(
+                            'float32')
+                        chunks.append(
+                            dict(blob=np_arr, weight=1.0, mime_type='image/png'))
 
-            # Extract text
-            with pdf_text:
-                count = len(pdf_text.pages)
-                for i in range(count):
-                    page = pdf_text.pages[i]
-                    text_page = page.extract_text(x_tolerance=1, y_tolerance=1)
-                    if text_page:
-                        chunks.append(dict(text=text_page, weight=1.0, mime_type='text/plain'))
+        # Extract text
+        with pdf_text:
+            count = len(pdf_text.pages)
+            for i in range(count):
+                page = pdf_text.pages[i]
+                text_page = page.extract_text(x_tolerance=1, y_tolerance=1)
+                if text_page:
+                    chunks.append(dict(text=text_page, weight=1.0, mime_type='text/plain'))
         return chunks
 
 
