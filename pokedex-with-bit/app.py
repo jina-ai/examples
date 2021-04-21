cd3b9d603da080a3b8dd0674f3e5c5a81042cfd0
slettner <sebastian.lettner@jina.ai>
2021-04-21 19:39:40 +0200

    feat: add click commands

--- pokedex-with-bit/app.py
+++ pokedex-with-bit/app.py
@@ -4,10 +4,11 @@ __license__ = "Apache-2.0"
 import os
 import sys
 
+import click
 from jina.flow import Flow
 
-num_docs = int(os.environ.get('JINA_MAX_DOCS', 50000))
-image_src = 'data/**/*.png'
+MAX_DOCS = int(os.environ.get('JINA_MAX_DOCS', 50000))
+IMAGE_SRC = 'data/**/*.png'
 
 
 def config():
@@ -20,39 +21,46 @@ def config():
     os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(45678))
 
 
-# for index
-def index():
+def index(num_docs: int):
     f = Flow.load_config('flows/index.yml')
 
     with f:
-        f.index_files(image_src, request_size=64, read_mode='rb', size=num_docs)
+        f.index_files(IMAGE_SRC, request_size=64, read_mode='rb', size=num_docs)
 
 
-# for search
-def search():
+def query():
     f = Flow.load_config('flows/query.yml')
+    f.use_rest_gateway()
 
     with f:
         f.block()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('choose between "index" and "search" mode')
-        exit(1)
-    if sys.argv[1] == 'index':
-        config()
-        workspace = os.environ['JINA_WORKSPACE']
+@click.command()
+@click.option(
+    "--task",
+    "-t",
+    type=click.Choice(
+        ["index", "query"], case_sensitive=False
+    ),
+)
+@click.option("--num_docs", "-n", default=MAX_DOCS)
+def main(task: str, num_docs: int):
+    config()
+    workspace = os.environ["JINA_WORKSPACE"]
+    if task == "index":
         if os.path.exists(workspace):
-            print(f'\n +---------------------------------------------------------------------------------+ \
-                    \n |                                   🤖🤖🤖                                        | \
-                    \n | The directory {workspace} already exists. Please remove it before indexing again. | \
-                    \n |                                   🤖🤖🤖                                        | \
-                    \n +---------------------------------------------------------------------------------+')
-            sys.exit()
-        index()
-    elif sys.argv[1] == 'search':
-        config()
-        search()
-    else:
-        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
+            print(f'\n +----------------------------------------------------------------------------------+ \
+                    \n |                                   🤖🤖🤖                                         | \
+                    \n | The directory {workspace} already exists. Please remove it before indexing again.  | \
+                    \n |                                   🤖🤖🤖                                         | \
+                    \n +----------------------------------------------------------------------------------+')
+        index(num_docs)
+    if task == "query":
+        if not os.path.exists(workspace):
+            print(f"The directory {workspace} does not exist. Please index first via `python app.py -t index`")
+        query()
+
+
+if __name__ == '__main__':
+    main()
