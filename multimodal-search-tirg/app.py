ff89907c8b9d344c9028b6f332dce51cd12594c5
rutujasurve94 <rutuja.r.surve@gmail.com>
2021-04-22 16:05:21 +0530

    fix: multi modal eg using CRUD REST

--- multimodal-search-tirg/app.py
+++ multimodal-search-tirg/app.py
@@ -3,13 +3,16 @@ __license__ = "Apache-2.0"
 
 import os
 import shutil
+import sys
 
 import click
+import requests
 import matplotlib.pyplot as plt
 
 from jina.flow import Flow
 from jina import Document
 from jina.clients.sugary_io import _input_files
+from jina.clients.sugary_io import _input_lines
 from jina.logging import default_logger as logger
 from jina.types.document.multimodal import MultimodalDocument
 
@@ -32,6 +35,35 @@ def config():
     os.makedirs(os.environ['JINA_WORKSPACE'], exist_ok=True)
     os.environ['JINA_PORT'] = '45678'
 
+
+def index_restful(num_docs):
+    f = Flow().load_config('flows/index.yml')
+
+    with f:
+        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE', None))
+        url = f'http://0.0.0.0:{f.port_expose}/index'
+
+        input_docs = _input_lines(
+            filepath=data_path,
+            size=num_docs,
+            read_mode='r',
+        )
+        data_json = {'data': [Document(text=text).dict() for text in input_docs]}
+        r = requests.post(url, json=data_json)
+        if r.status_code != 200:
+            raise Exception(f'api request failed, url: {url}, status: {r.status_code}, content: {r.content}')
+
+
+def query_restful():
+    f = Flow().load_config("flows/query.yml")
+    f.use_rest_gateway()
+    with f:
+        f.block()
+
+def dryrun():
+    f = Flow().load_config("flows/index.yml")
+    with f:
+        pass
 
 def plot_topk_images(images):
     n_row = 1
@@ -78,7 +110,7 @@ def query_generator(image_paths, text_queries):
 
 
 @click.command()
-@click.option('--task', '-t', type=click.Choice(['index', 'query'], case_sensitive=False))
+@click.option('--task', '-t', type=click.Choice(['index', 'query', 'index_restful', 'query_restful', 'dryrun'], case_sensitive=False))
 @click.option('--data_path', '-p', default=data_path)
 @click.option('--num_docs', '-n', default=num_docs)
 @click.option('--batch_size', '-b', default=batch_size)
@@ -89,16 +121,36 @@ def main(task, data_path, num_docs, batch_size, image_path, text_query, overwrit
     config()
     image_paths = [image_path]
     text_queries = [text_query]
+    workspace = os.environ["JINA_WORKSPACE"]
+    if 'index' in task:
+        if os.path.exists(workspace):
+            print(
+                f'\n +------------------------------------------------------------------------------------+ \
+                    \n |                                   🤖🤖🤖                                           | \
+                    \n | The directory {workspace} already exists. Please remove it before indexing again.  | \
+                    \n |                                   🤖🤖🤖                                           | \
+                    \n +------------------------------------------------------------------------------------+'
+            )
+            sys.exit(1)
+
     if task == 'index':
         if overwrite_workspace:
             clean_workdir()
         f = Flow.load_config('flow-index.yml')
         with f:
             f.index(index_generator(data_path, num_docs), batch_size=batch_size)
+    elif task == 'index_restful':
+        index_restful(num_docs)
     elif task == 'query':
         f = Flow.load_config('flow-query.yml')
         with f:
             f.search(input_fn=query_generator(image_paths, text_queries), on_done=print_result)
+    elif task == 'query_restful':
+        if not os.path.exists(workspace):
+            print(f"The directory {workspace} does not exist. Please index first via `python app.py -t index`")
+        query_restful()
+    elif task == 'dryrun':
+        dryrun()
 
 
 if __name__ == '__main__':
