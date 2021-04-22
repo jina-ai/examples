41e28ff146723e42ebc98587278564efefa35ff6
Yongxuan Zhang <44033547+Yongxuanzhang@users.noreply.github.com>
2021-04-22 03:08:15 -0400

    multimodal search pdf (#439)
    * feat[mmpdf]: pdf search draft
    
    * feat[mmpdf]: code refactoring
    
    * feat[mmpdf]: fix text segment
    
    * feat[mmpdf]: fix mimetype
    
    * feat[mmpdf]: divide segmenters
    
    * feat[mmpdf]: new pdf extractor
    
    * feat[mmpdf]: parallel segmenter
    
    * feat[mmpdf]: deeper chunks
    
    * fix: split indexer into c and cc
    
    * feat[mmpdf]: separate text query
    
    * feat[mmpdf]: fix mminput
    
    * feat[mmpdf]: fix ranker
    
    * feat[mmpdf]: clean code
    
    * feat[mmpdf]: clean code
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: clean code
    
    * feat[mmpdf]: fix none text
    
    * feat[mmpdf]: fix channels
    
    * feat[mmpdf]: add parallel
    
    * feat[mmpdf]: address comments
    
    * feat[mmpdf]: rename file
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: fix generator
    
    * feat[mmpdf]: add index generator
    
    * feat[mmpdf]: add docker file
    
    * feat[mmpdf]: clean app
    
    * feat[mmpdf]: add test
    
    * feat[mmpdf]: add rest endpoint
    
    * feat[mmpdf]: add manifest
    
    * feat[mmpdf]: add ci
    
    * feat[mmpdf]: add toy data
    
    * feat[mmpdf]: add toy data
    
    * feat[mmpdf]: remove pypdf2
    
    * feat[mmpdf]: loose test
    
    * feat[mmpdf]: pump version
    
    * feat[mmpdf]: add more docstrings
    
    * feat[mmpdf]: fix dryrun
    
    * feat[mmpdf]: fix rest
    
    * feat[mmpdf]: add doc
    
    * feat[mmpdf]: add readme
    
    * feat[mmpdf]: fix jpeg
    
    * feat[mmpdf]: fix image seg
    
    * feat[mmpdf]: add docstrings
    
    * feat[mmpdf]: fix segmenter
    
    * feat[mmpdf]: fix image segmenter
    
    * feat[mmpdf]: add readme
    
    * feat[mmpdf]: add readme
    
    * feat[mmpdf]: add readme
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * feat[mmpdf]: fix errors
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/segmenters.py
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/Dockerfile
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/README.md
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * feat[mmpdf]: fix readme
    
    * Update multimodal-search-pdf/pods/rankc.yml
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * Update multimodal-search-pdf/pods/index-doc.yml
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    * feat[mmpdf]: fix readme
    
    * feat[mmpdf]: fix readme
    
    * feat[mmpdf]: fix readme
    
    * feat[mmpdf]: fix yaml
    
    * feat[mmpdf]: fix readme
    
    * feat[mmpdf]: fix readme
    
    * feat[mmpdf]: change parallel
    
    * feat[mmpdf]: remove unused code
    
    * feat[mmpdf]: remove parallel
    
    * feat[mmpdf]: add parallel for test
    
    * feat[mmpdf]: add conftest
    
    * test: add get data
    
    * test: add summary for readme
    
    * test: add performance metrics
    
    Co-authored-by: Maximilian Werk <maximilian.werk@jina.ai>
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

---
+++ multimodal-search-pdf/get_data.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import shutil
+import weasyprint
+
+blog_file = 'toy_data/blog-pdf.csv'
+
+try:
+    shutil.rmtree('data')
+    os.makedirs('data')
+except:
+    pass
+
+with open(blog_file, newline='', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        if row[0] == 'Name':
+            continue
+        name = 'data/' + row[0] + '.pdf'
+        url = row[1]
+        pdf = weasyprint.HTML(url).write_pdf()
+        with open(name, 'wb') as outputStream:
+            outputStream.write(pdf)
