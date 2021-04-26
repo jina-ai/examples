2ac066b3baae13bd7feff510ffae4c4fe2a33a6b
rutujasurve94 <rutuja.r.surve@gmail.com>
2021-04-26 12:49:17 +0530

    fix: signature of fashion encode

--- fashion-example-query/components.py
+++ fashion-example-query/components.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import numpy as np
@@ -19,6 +19,6 @@ class MyEncoder(BaseImageEncoder):
         self.oth_mat = u @ vh
         self.touch()
 
-    def encode(self, data: 'np.ndarray', *args, **kwargs):
+    def encode(self, content: 'np.ndarray', *args, **kwargs):
         # reduce dimension to 50 by random orthogonal projection
-        return (data.reshape([-1, 784]) / 255) @ self.oth_mat
+        return (content.reshape([-1, 784]) / 255) @ self.oth_mat
