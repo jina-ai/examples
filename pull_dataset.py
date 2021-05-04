c127b9eaa7449b83dd4cfe24b08798e737faf77a
slettner <sebastian.lettner@jina.ai>
2021-05-04 11:21:18 +0200

    feat: safety check for credentials

--- pull_dataset.py
+++ pull_dataset.py
@@ -10,6 +10,13 @@ BUCKET_NAME = "jina-examples-datasets"
 log = logging.getLogger(__name__)
 
 
+def _check_credentials_exist():
+    assert os.environ.get('AWS_ACCESS_KEY_ID') is not None, 'Missing access key'
+    assert len(os.environ['AWS_ACCESS_KEY_ID']) > 0, 'Empty access key'
+    assert os.environ.get('AWS_SECRET_ACCESS_KEY') is not None, 'Missing secret key'
+    assert len(os.environ['AWS_SECRET_ACCESS_KEY']) > 0, 'Empty secret key'
+
+
 @click.command()
 @click.option(
     "--data-set",
@@ -24,7 +31,8 @@ log = logging.getLogger(__name__)
     required=True
 )
 def main(data_set: str, pull_to_dir: str):
-    assert os.path.isdir(pull_to_dir), "The pull dir parameter must be a directory"
+    _check_credentials_exist()
+    assert os.path.isdir(pull_to_dir), "The pull dir parameter must be an existing directory"
     save_path = os.path.join(pull_to_dir, data_set)
     s3 = S3(BUCKET_NAME)
     try:
