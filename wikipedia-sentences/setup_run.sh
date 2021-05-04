9dea6d4d4c5fed2d7f5d6f8f0e9eb14c0da86588
slettner <sebastian.lettner@jina.ai>
2021-05-04 10:44:43 +0200

    feat: add s3 download

--- wikipedia-sentences/setup_run.sh
+++ wikipedia-sentences/setup_run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+DATA_DIR='data'
+DATA_FILE='input.txt'
+
+if [ ! -f "$DATA_DIR/$DATA_FILE" ]; then
+  echo 'no data file found. starting download, this may take a while...'
+  python ../pull_dataset.py -d ${DATA_FILE} -p ${DATA_DIR}
+fi
+
 rm -rf workspace && \
-python app.py -t index | tee metrics.txt && \
+export JINA_DATA_FILE="$DATA_DIR/$DATA_FILE" && \
+python app.py -t index -n 5000 | tee metrics.txt && \
 python app.py -t query <<< 'some text from stdin' | tee >> metrics.txt
