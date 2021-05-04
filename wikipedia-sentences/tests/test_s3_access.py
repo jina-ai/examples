b13b506b8cc84f4bd8d3ff7744353e364dfc59e5
Jacobowitz <tobias.jacobowitz@jina.ai>
2021-05-04 10:53:04 +0200

    assert secret key exists

--- wikipedia-sentences/tests/test_s3_access.py
+++ wikipedia-sentences/tests/test_s3_access.py
@@ -7,6 +7,7 @@ def test_s3_access():
     assert not os.path.isfile(path='./dummy_test_file')
     assert "AWS_ACCESS_KEY_ID" in os.environ
     assert "AWS_SECRET_ACCESS_KEY" in os.environ
+    assert len(os.environ["AWS_SECRET_ACCESS_KEY"]) > 0
 
     S3Bucket(bucket_name='jina-examples-datasets').get(key='dummy_test_file', local_path='./dummy_test_file')
     assert os.path.isfile(path='./dummy_test_file')
