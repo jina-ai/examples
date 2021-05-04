7752bf7136c8034af7a4bd3d13c77670ffc3d867
Jacobowitz <tobias.jacobowitz@jina.ai>
2021-05-04 09:45:46 +0200

    try if s3 access works in ci

---
+++ wikipedia-sentences/tests/test_s3_access.py
@@ -0,0 +1,12 @@
+import os
+
+from jinacld_tools.aws.services.s3 import S3Bucket
+
+
+def test_s3_access():
+    assert not os.path.isfile(path='./dummy_test_file')
+    assert "AWS_ACCESS_KEY_ID" in os.environ
+    assert "AWS_SECRET_ACCESS_KEY" in os.environ
+
+    S3Bucket(bucket_name='jina-examples-datasets').get(key='dummy_test_file', local_path='./dummy_test_file')
+    assert os.path.isfile(path='./dummy_test_file')
