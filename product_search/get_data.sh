2318ace93eb36334205364b2da1dfb70c34a83e8
Alex C-G <alexcg@outlook.com>
2021-05-10 11:44:15 +0200

    fix(product-search): rm headers from dataset

--- product_search/get_data.sh
+++ product_search/get_data.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 DATASET="vikashrajluhaniwal/fashion-images"
+FILENAME="fashion.csv"
 
 # Download and unzip dataset (defaults to data/ dir)
 kaggle datasets download -d ${DATASET} --unzip
+
+# Remove first line (column titles)
+cd data
+echo "$(tail -n +2 ${FILENAME})" > ${FILENAME}
