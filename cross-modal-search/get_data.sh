63636df4f3747839af07ad27fd1d3a299a0d784e
Jakob Kruse <jakob.kruse@jina.ai>
2021-05-19 16:29:14 +0200

    docs: improve data download

--- cross-modal-search/get_data.sh
+++ cross-modal-search/get_data.sh
@@ -7,12 +7,11 @@ if [ -d ${DATA_DIR} ]; then
   exit 1
 fi
 
-mkdir -p ${DATA_DIR}
-kaggle datasets download -d ${DATASET}
-unzip flickr8k.zip && \
+mkdir -p ${DATA_DIR} && \
+kaggle datasets download -d ${DATASET} && \
+unzip -q flickr8k.zip && \
 rm flickr8k.zip && \
-mkdir data || true && \
-mkdir data/f8k || true && \
-mkdir data/images || true && \
+mkdir -p data && \
+mkdir -p data/f8k && \
 mv Images data/f8k/images && \
 mv captions.txt data/f8k/captions.txt
