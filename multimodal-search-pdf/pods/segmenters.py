74d86d9dca444945697280a949d7b5c7211858bf
slettner <sebastian.lettner@jina.ai>
2021-05-26 14:37:34 +0200

    docs: segmenters to 2.0

--- multimodal-search-pdf/pods/segmenters.py
+++ multimodal-search-pdf/pods/segmenters.py
@@ -1,21 +1,26 @@
 import io
-from typing import Dict, List
 
+import fitz
+import pdfplumber
 import numpy as np
-from jina.executors.segmenters import BaseSegmenter
-from jina.executors.decorators import single, batching
+from PIL import Image
+
+from jina import Executor, requests, DocumentArray, Document
+from jina.logging import JinaLogger
 
 
-class PDFSegmenter(BaseSegmenter):
+class LoggerMixing:
+    def __init__(self):
+        self.logger = JinaLogger(self.__class__.__name__)
+
+
+class PDFSegmenter(Executor, LoggerMixing):
     """
     :class:`PDFSegmenter` Extracts data (text and images) from PDF files.
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @single(slice_nargs=3)
-    def segment(self, uri: str, buffer: bytes, mime_type: str, *args, **kwargs) -> List[Dict]:
+    @requests(on=['index', 'search'])
+    def segment(self, docs: DocumentArray, **kwargs):
         """
         Segements PDF files. Extracts data from them.
 
@@ -24,39 +29,51 @@ class PDFSegmenter(BaseSegmenter):
         It will then extract the data from the file, creating a list for images,
         and text.
 
-        :param uri: File name of PDF
-        :type uri: str
-        :param buffer: PDF file in bytes
-        :type buffer: bytes
-        :param mime_type: the type of data
-        :returns: A list of documents with the extracted data
-        :rtype: List[Dict]
+        :param docs: Array of Documents.
+
         """
-        chunks = []
-        if mime_type != 'application/pdf':
-            return chunks
-        import fitz  # fitz is a library used in `PyMuPDF` to read pdf and images
-        import pdfplumber
+        docs = PDFSegmenter._filter_pdf(docs)
+        for doc in docs:
+            pdf_img, pdf_text = self._parse_pdf(doc)
 
-        if uri:
-            try:
-                pdf_img = fitz.open(uri)
-                pdf_text = pdfplumber.open(uri)
-            except Exception as ex:
-                self.logger.error(f'Failed to open {uri}: {ex}')
-                return chunks
-        elif buffer:
-            try:
-                pdf_img = fitz.open(stream=buffer, filetype='pdf')
-                pdf_text = pdfplumber.open(io.BytesIO(buffer))
-            except Exception as ex:
-                self.logger.error(f'Failed to load from buffer')
-                return chunks
-            else:
-                self.logger.error('No value found in `buffer` or `uri`')
-            return chunks
+            # Extract images
+            if pdf_img is not None:
+                self._extract_image(doc, pdf_img)
 
-        # Extract images
+            if pdf_text is not None:
+                self._extract_text(doc, pdf_text)
+
+    @staticmethod
+    def _filter_pdf(docs: DocumentArray) -> DocumentArray:
+        return DocumentArray([doc for doc in docs if doc.mime_type == 'application/pdf'])
+
+    def _parse_pdf(self, doc: Document):
+        pdf_img = None
+        pdf_text = None
+        try:
+            if doc.uri:
+                pdf_img = fitz.open(doc.uri)
+                pdf_text = pdfplumber.open(doc.uri)
+            if doc.buffer:
+                pdf_img = fitz.open(stream=doc.buffer, filetype='pdf')
+                pdf_text = pdfplumber.open(io.BytesIO(doc.buffer))
+        except Exception as ex:
+            self.logger.error(f'Failed to open due to: {ex}')
+        return pdf_img, pdf_text
+
+    @staticmethod
+    def _extract_text(doc, pdf_text):
+        # Extract text
+        with pdf_text:
+            count = len(pdf_text.pages)
+            for i in range(count):
+                page = pdf_text.pages[i]
+                text_page = page.extract_text(x_tolerance=1, y_tolerance=1)
+                if text_page:
+                    doc.chunks.append(Document(text=text_page, weight=1.0, mime_type='text/plain'))
+
+    @staticmethod
+    def _extract_image(doc, pdf_img):
         with pdf_img:
             for page in range(len(pdf_img)):
                 for img in pdf_img.getPageImageList(page):
@@ -65,128 +82,47 @@ class PDFSegmenter(BaseSegmenter):
                     # read data from buffer and reshape the array into 3-d format
                     np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype('float32')
                     if pix.n - pix.alpha < 4:  # if gray or RGB
-                        if pix.n == 1: #convert gray to rgb
-                            np_arr_rgb = np.concatenate((np_arr,)*3,-1)
-                            chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
-                        elif pix.n == 4: # remove transparency layer
+                        if pix.n == 1:  # convert gray to rgb
+                            np_arr_rgb = np.concatenate((np_arr,) * 3, -1)
+                            doc.chunks.append(Document(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
+                        elif pix.n == 4:  # remove transparency layer
                             np_arr_rgb = np_arr[..., :3]
-                            chunks.append(dict(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
+                            doc.chunks.append(Document(blob=np_arr_rgb, weight=1.0, mime_type='image/png'))
                         else:
-                            chunks.append(dict(blob=np_arr, weight=1.0, mime_type='image/png'))
+                            doc.chunks.append(Document(blob=np_arr, weight=1.0, mime_type='image/png'))
                     else:  # if CMYK:
                         pix = fitz.Pixmap(fitz.csRGB, pix)  # Convert to RGB
                         np_arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n).astype(
                             'float32')
-                        chunks.append(
-                            dict(blob=np_arr, weight=1.0, mime_type='image/png'))
-
-        # Extract text
-        with pdf_text:
-            count = len(pdf_text.pages)
-            for i in range(count):
-                page = pdf_text.pages[i]
-                text_page = page.extract_text(x_tolerance=1, y_tolerance=1)
-                if text_page:
-                    chunks.append(dict(text=text_page, weight=1.0, mime_type='text/plain'))
-        return chunks
+                        doc.chunks.append(
+                            Document(blob=np_arr, weight=1.0, mime_type='image/png'))
 
 
-class TextSegmenter(BaseSegmenter):
-    """
-    :class:`TextSegmenter` simply store text into chunks.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @single(slice_nargs=4)
-    def segment(self, text: str, uri: str, buffer: bytes, mime_type: str, *args, **kwargs) -> List[Dict]:
-        """
-        Segements text.
-
-        :param text: the text data
-        :param uri: File name of PDF
-        :type uri: str
-        :param buffer: PDF file in bytes
-        :type buffer: bytes
-        :param mime_type: the type of data
-        :returns: A list of documents with the extracted data
-        :rtype: List[Dict]
-        """
-
-        chunks = []
-        if mime_type == 'text/plain':
-            chunks.append(dict(text=text, weight=1.0, mime_type='text/plain'))
-        return chunks
-
-
-class CustomizedTextSegmenter(BaseSegmenter):
+class TextIntoLinesSegmenter(Executor, LoggerMixing):
     """
     :class:`TextSegmenterCustomized` segments text into lines
     """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @single(slice_nargs=4)
-    def segment(self, text: str, uri: str, buffer: bytes, mime_type: str, *args, **kwargs) -> List[Dict]:
-        """
-        Segements text.
-
-        :param text: the text data
-        :param uri: File name of PDF
-        :type uri: str
-        :param buffer: PDF file in bytes
-        :type buffer: bytes
-        :param mime_type: the type of data
-        :returns: A list of documents with the extracted data
-        :rtype: List[Dict]
-        """
-        chunks = []
-        if mime_type == 'text/plain':
-            temp = text.split('\n')
-            for t in temp:
-                chunks.append(dict(text=t, weight=1.0, mime_type='text/plain'))
-        return chunks
+    @requests(on=['index', 'search'])
+    def segment(self, docs: DocumentArray, **kwargs):
+        for doc in docs:
+            if doc.mime_type == 'text/plain':
+                doc.chunks += [Document(text=t, weight=1.0, mime_type='text/plain') for t in doc.text.split('\n')]
 
 
-class ImageSegmenter(BaseSegmenter):
+class ImageSegmenter(Executor, LoggerMixing):
     """
     :class:`ImageSegmenter` reads image and stores into chunks.
     """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @single(slice_nargs=3)
-    def segment(self,  uri: str, buffer: bytes, mime_type: str, *args, **kwargs) -> List[Dict]:
-        """
-        Segements image.
-
-        :param uri: File name of image
-        :type uri: str
-        :param buffer: image file in bytes
-        :type buffer: bytes
-        :param mime_type: the type of data
-        :returns: A list of documents with the extracted data
-        :rtype: List[Dict]
-        """
-        chunks=[]
-        if mime_type.startswith('image'):
-            from PIL import Image
-            self.channel_axis: int = -1
-
-            if buffer:
-                raw_img = Image.open(io.BytesIO(buffer))
-            elif uri:
-                raw_img = Image.open(uri)
-            else:
-                raise ValueError('no value found in "buffer" and "uri"')
-            raw_img = raw_img.convert('RGB')
-            img = np.array(raw_img).astype('float32')
-            if self.channel_axis != -1:
-                img = np.moveaxis(img, -1, self.channel_axis)
-
-            chunks = [dict(blob=img, weight=1.0, mime_type='image/png')]
-            return chunks
-        return chunks
+    @requests(on=['index', 'search'])
+    def segment(self, docs: DocumentArray, **kwargs):
+        for doc in docs:
+            if doc.mime_type.startswith('image'):
+                if doc.buffer:
+                    raw_img = Image.open(io.BytesIO(doc.buffer))
+                elif doc.uri:
+                    raw_img = Image.open(doc.uri)
+                else:
+                    raise ValueError('no value found in "buffer" and "uri"')
+                raw_img = raw_img.convert('RGB')
+                img = np.array(raw_img).astype('float32')
+                doc.chunks.append(Document(blob=img, weight=1.0, mime_type='image/png'))
---
+++ multimodal-search-pdf/tests/test_segmenter.py
@@ -0,0 +1,43 @@
+# Test the segmenter.py implementations
+
+from jina import Document, DocumentArray
+
+from pods.segmenters import PDFSegmenter, ImageSegmenter, TextIntoLinesSegmenter
+
+
+def test_text_into_line_segmenter() -> None:
+    docs = DocumentArray([Document(content='foo bar\nbar foo'), Document(content='hyper text')])
+    segmenter = TextIntoLinesSegmenter()
+
+    segmenter.segment(docs)
+
+    assert len(docs[0].chunks) == 2
+    assert len(docs[1].chunks) == 1
+
+
+def test_image_segmenter() -> None:
+    docs = DocumentArray([Document(uri='../toy_data/photo-1.png')])
+    segmenter = ImageSegmenter()
+
+    segmenter.segment(docs)
+
+    assert len(docs[0].chunks) == 1
+
+
+def test_pdf_segmenter_creates_chunks() -> None:
+    docs = DocumentArray([Document(uri='../toy_data/blog1.pdf')])
+    segmenter = PDFSegmenter()
+
+    segmenter.segment(docs)
+
+    assert len(docs[0].chunks) > 0, 'Expected more than zero chunks to be extracted from the PDF'
+
+
+def test_pdf_segmenter_creates_mime_types() -> None:
+    docs = DocumentArray([Document(uri='../toy_data/blog1.pdf')])
+    segmenter = PDFSegmenter()
+
+    segmenter.segment(docs)
+
+    for chunk in docs[0].chunks:
+        assert chunk.mime_type in ['image/png', 'text/plain']
