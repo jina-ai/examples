5de10fe1a4bf2baaf4980b00daec3a62ced39a70
jakob1111996 <jakob.kruse@jina.ai>
2021-06-09 17:14:10 +0200

    refactor: update transformer torch encoder to 2.0

--- multires-lyrics-search/flows/executors.py
+++ multires-lyrics-search/flows/executors.py
@@ -1,8 +1,10 @@
 """ Executors required for the multires example """
 
 import re
-import string
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union, Tuple, Iterable
+import numpy as np
+import torch
+from transformers import AutoModel, AutoTokenizer
 
 from jina import Executor, DocumentArray, Document, requests
 from jina.logging.predefined import default_logger as logger
@@ -74,3 +76,73 @@ class Sentencizer(Executor):
                             location=[s, e])
                     )
         return docs
+
+
+class TransformerTorchEncoder(Executor):
+    """Transformer executor class """
+    def __init__(
+        self,
+        pretrained_model_name_or_path: str = 'sentence-transformers/distilbert-base-nli-stsb-mean-tokens',
+        base_tokenizer_model: Optional[str] = None,
+        pooling_strategy: str = 'mean',
+        layer_index: int = -1,
+        max_length: Optional[int] = None,
+        acceleration: Optional[str] = None,
+        embedding_fn_name: str = '__call__',
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.base_tokenizer_model = (
+            base_tokenizer_model or pretrained_model_name_or_path
+        )
+        self.pooling_strategy = pooling_strategy
+        self.layer_index = layer_index
+        self.max_length = max_length
+        self.acceleration = acceleration
+        self.embedding_fn_name = embedding_fn_name
+        self.tokenizer = AutoTokenizer.from_pretrained(self.base_tokenizer_model)
+        self.model = AutoModel.from_pretrained(
+            self.pretrained_model_name_or_path, output_hidden_states=True
+        )
+        self.model.to(torch.device('cpu'))
+
+    def _compute_embedding(self, hidden_states: 'torch.Tensor', input_tokens: Dict):
+        fill_vals = {'cls': 0.0, 'mean': 0.0, 'max': -np.inf, 'min': np.inf}
+        fill_val = torch.tensor(
+            fill_vals[self.pooling_strategy], device=torch.device('cpu')
+        )
+        layer = hidden_states[self.layer_index]
+        attn_mask = input_tokens['attention_mask'].unsqueeze(-1).expand_as(layer)
+        layer = torch.where(attn_mask.bool(), layer, fill_val)
+        embeddings = layer.sum(dim=1) / attn_mask.sum(dim=1)
+        return embeddings.cpu().numpy()
+
+    @requests
+    def encode(self, docs: 'DocumentArray', **kwargs):
+        chunks = docs.traverse_flat(['c'])
+        texts = chunks.get_attributes('text')
+
+        with torch.no_grad():
+            if not self.tokenizer.pad_token:
+                self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+                self.model.resize_token_embeddings(len(self.tokenizer.vocab))
+            input_tokens = self.tokenizer(
+                texts,
+                max_length=self.max_length,
+                padding='longest',
+                truncation=True,
+                return_tensors='pt',
+            )
+            input_tokens = {
+                k: v.to(torch.device('cpu')) for k, v in input_tokens.items()
+            }
+            outputs = getattr(self.model, self.embedding_fn_name)(**input_tokens)
+            if isinstance(outputs, torch.Tensor):
+                return outputs.cpu().numpy()
+            hidden_states = outputs.hidden_states
+            embeds = self._compute_embedding(hidden_states, input_tokens)
+            for doc, embed in zip(chunks, embeds):
+                doc.embedding = embed
+        return chunks
