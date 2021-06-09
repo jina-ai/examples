0ffdc5493769bd9ea0ded714b5937180802b8aa8
jakob1111996 <jakob.kruse@jina.ai>
2021-06-09 16:50:59 +0200

    refactor: upgrade sentencizer to 2.0

--- multires-lyrics-search/app.py
+++ multires-lyrics-search/app.py
@@ -6,19 +6,18 @@ __version__ = '0.0.1'
 import os
 import sys
 import click
+import requests
 
-from jina import Flow, Document
+from jina.flow import Flow
+from jina import Document
 from helper import input_generator
 from jina.logging.predefined import default_logger as logger
+# from jina.logging.profile import TimeContext
 
 
 def config():
     os.environ.setdefault('JINA_WORKSPACE', './workspace')
-    os.environ.setdefault('JINA_LOG_LEVEL', 'DEBUG')
-    if os.path.exists('lyrics-data/lyrics-data.csv'):
-        os.environ.setdefault('JINA_DATA_FILE', 'lyrics-data/lyrics-data.csv')
-    else:
-        os.environ.setdefault('JINA_DATA_FILE', 'lyrics-data/lyrics-toy-data1000.csv')
+    os.environ.setdefault('JINA_DATA_FILE', 'toy-data/lyrics-toy-data1000.csv')
     os.environ.setdefault('JINA_PORT', str(45678))
 
 
@@ -27,10 +26,23 @@ def index(num_docs):
     flow = Flow.load_config('flows/index.yml')
     with flow:
         input_docs = input_generator(num_docs=num_docs)
-        data_path = os.path.join(os.path.dirname(__file__),
-                                 os.environ.get('JINA_DATA_FILE', None))
-        flow.logger.info(f'Indexing {data_path}')
+        # with TimeContext(f'QPS: indexing {len(input_docs)}', logger=flow.logger):
         flow.post(on='/index', inputs=input_docs, request_size=10)
+
+
+def index_restful(num_docs):
+    f = Flow().load_config('flows/index.yml')
+    f.use_rest_gateway()
+    with f:
+        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_FILE', None))
+        f.logger.info(f'Indexing {data_path}')
+        url = f'http://0.0.0.0:{f.port_expose}/index'
+
+        input_docs = input_generator(num_docs=num_docs)
+        data_json = {'data': [doc.dict() for doc in input_docs]}
+        r = requests.post(url, json=data_json)
+        if r.status_code != 200:
+            raise Exception(f'api request failed, url: {url}, status: {r.status_code}, content: {r.content}')
 
 
 # for search
@@ -40,35 +52,21 @@ def query():
         f.block()
 
 
-def query_text():
-    def print_result(response):
-        print(f'### Number of response documents: {len(response.docs)}')
-
-        print(f'### Total matches {sum([len(d.matches) for d in response.docs])}')
-        for i, match in enumerate(response.docs):
-            print(f'###\tMatch {i}: {match.matches[0].text}')
-
-    f = Flow.load_config('flows/query.yml')
-    with f:
-        search_text = input('Please type a sentence: ')
-        doc = Document(content=search_text, mime_type='text/plain')
-        f.post('/search', inputs=doc, on_done=print_result)
-
-
 def query_restful():
-    flow = Flow.load_config("flows/query.yml")
-    flow.protocol = 'http'
-    with flow:
-        flow.block()
+    f = Flow().load_config("flows/query.yml")
+    f.use_rest_gateway()
+    with f:
+        f.block()
 
 
 @click.command()
 @click.option('--task', '-t',
-              type=click.Choice(['index', 'query', 'query_restful', 'query_text'], case_sensitive=False))
-@click.option('--num_docs', '-n', default=10000)
+              type=click.Choice(['index', 'query', 'index_restful', 'query_restful'], case_sensitive=False))
+@click.option('--num_docs', '-n', default=1000)
 def main(task, num_docs):
     config()
     workspace = os.environ["JINA_WORKSPACE"]
+
     if task == 'index':
         if os.path.exists(workspace):
             logger.error(f'\n +---------------------------------------------------------------------------------+ \
@@ -78,17 +76,17 @@ def main(task, num_docs):
                     \n +---------------------------------------------------------------------------------+')
             sys.exit(1)
         index(num_docs)
+    elif task == 'index_restful':
+        index_restful(num_docs)
     elif task == 'query':
         query()
-    elif task == 'query_text':
-        query_text()
     elif task == 'query_restful':
         if not os.path.exists(workspace):
             logger.warning(f'The directory {workspace} does not exist. Please index first via `python app.py -t index`')
         query_restful()
     else:
         raise NotImplementedError(
-            f'Unknown task: {task}.')
+            f'unknown task: {task}. A valid task is either `index` or `query`.')
 
 
 if __name__ == '__main__':
--- multires-lyrics-search/helper.py
+++ multires-lyrics-search/helper.py
@@ -3,14 +3,14 @@
 import csv
 import itertools as it
 import os
-import numpy as np
 
 from jina import Document
 
 
 def input_generator(num_docs: int):
-    lyrics_file = os.environ.setdefault('JINA_DATA_FILE',
-                                        'lyrics-data/lyrics-toy-data1000.csv')
+    lyrics_file = os.environ.setdefault(
+        'JINA_DATA_PATH', 'toy-data/lyrics-toy-data1000.csv'
+    )
     with open(lyrics_file, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in it.islice(reader, num_docs):
@@ -21,37 +21,3 @@ def input_generator(num_docs: int):
                     d.tags['SLink'] = row[2]
                     d.text = row[3]
                     yield d
-
-
-def num_input_docs():
-    lyrics_file = os.environ.setdefault(
-        'JINA_DATA_PATH', 'lyrics-data/lyrics-toy-data1000.csv'
-    )
-    with open(lyrics_file, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        return len(list(reader))
-
-def _ext_A(A):
-    nA, dim = A.shape
-    A_ext = np.ones((nA, dim * 3))
-    A_ext[:, dim : 2 * dim] = A
-    A_ext[:, 2 * dim :] = A ** 2
-    return A_ext
-
-
-def _ext_B(B):
-    nB, dim = B.shape
-    B_ext = np.ones((dim * 3, nB))
-    B_ext[:dim] = (B ** 2).T
-    B_ext[dim : 2 * dim] = -2.0 * B.T
-    del B
-    return B_ext
-
-
-def _norm(A):
-    return A / np.linalg.norm(A, ord=2, axis=1, keepdims=True)
-
-
-def _euclidean(A_ext, B_ext):
-    sqdist = A_ext.dot(B_ext).clip(min=0)
-    return np.sqrt(sqdist)
