3553b6c581f741bb5419d3a35b6fe67ca4aef9d4
bwanglzu <bo.wang@jina.ai>
2021-07-01 12:23:06 +0200

    feat: update script and dependency

--- multires-lyrics-search/get_data.sh
+++ multires-lyrics-search/get_data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+mkdir .workspace
 kaggle datasets download -d neisse/scrapped-lyrics-from-6-genres
-unzip scrapped-lyrics-from-6-genres.zip
+unzip scrapped-lyrics-from-6-genres.zip -d .workspace
 rm -rf scrapped-lyrics-from-6-genres.zip
-rm -rf artists-data.csv 
+rm -rf .workspace/artists-data.csv
