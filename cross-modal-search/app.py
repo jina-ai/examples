0c38050f8293a3b6ebc3f3bb0f4460215a710a84
Winston Wong <winstonwong@Winstons-MacBook-Pro.local>
2021-07-08 11:42:51 +0800

    refactor: port ImageNormalizer to cross-modal-search

--- cross-modal-search/app.py
+++ cross-modal-search/app.py
@@ -57,7 +57,6 @@ def index(data_set, num_docs, request_size):
 
 def query():
     flow = Flow().load_config('flows/flow-query.yml')
-    flow.use_rest_gateway()
     with flow:
         flow.search(inputs=[
             Document(text='a black dog and a spotted dog are fighting', modality='text'),
